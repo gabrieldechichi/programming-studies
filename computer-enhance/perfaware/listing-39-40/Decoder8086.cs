9bb027bc285e7f306bd9a2cc91d4c4c2d11109a3
Gabriel Dechichi <gabrieldechichi@gmail.com>
2023-04-02 21:06:45 -0700

    Complete listing 40

--- computer-enhance/perfaware/listing-39-40/Decoder8086.cs
+++ computer-enhance/perfaware/listing-39-40/Decoder8086.cs
@@ -2,13 +2,9 @@ using System.Text;
 
 public static class Decoder8086
 {
-    private const byte REG_MASK = 0b0011_1000;
-    private const byte RM_MASK = 0b0111;
-
-    private const byte MOD_MASK = 0b1100_0000;
-
-    private static readonly string[] RegTable_W0 = new string[]
+    private static readonly string[] RegTable = new string[]
     {
+        //w = 0
         "al",
         "cl",
         "dl",
@@ -17,10 +13,8 @@ public static class Decoder8086
         "ch",
         "dh",
         "bh",
-    };
 
-    private static readonly string[] RegTable_W1 = new string[]
-    {
+        //w = 1
         "ax",
         "cx",
         "dx",
@@ -50,20 +44,20 @@ public static class Decoder8086
         strBuilder.AppendLine("bits 16");
         strBuilder.AppendLine("");
 
-        ushort readWord()
+        short readWord()
         {
-            return (ushort)(bytes[index++] | (bytes[index++] << 8));
+            return (short)(bytes[index++] | (bytes[index++] << 8));
         }
 
-        ushort readByte()
+        sbyte readByte()
         {
-            return bytes[index++];
+            return (sbyte)bytes[index++];
         }
 
         string decodeEffectAddressCalculation(byte b)
         {
-            var mod = (b & MOD_MASK) >> 6;
-            var rm = b & RM_MASK;
+            var mod = (b & 0b1100_0000) >> 6;
+            var rm = b & 0b0111;
             var displacementOrDirectAddress = mod switch
             {
                 //direct address (16 bit)
@@ -75,9 +69,17 @@ public static class Decoder8086
                 _ => 0
             };
 
-            return displacementOrDirectAddress > 0
-                ? $"[{MODOperandsTable[rm]} + {displacementOrDirectAddress}]"
-                : $"[{MODOperandsTable[rm]}]";
+            if (mod == 0b00 && rm == 0b110)
+            {
+                return $"[{displacementOrDirectAddress}]";
+            }
+
+            return displacementOrDirectAddress switch
+            {
+                0 => $"[{MODOperandsTable[rm]}]",
+                > 0 => $"[{MODOperandsTable[rm]} + {displacementOrDirectAddress}]",
+                _ => $"[{MODOperandsTable[rm]} - {-1 * displacementOrDirectAddress}]"
+            };
         }
 
         while (index < bytes.Length)
@@ -87,19 +89,19 @@ public static class Decoder8086
             //Register/memory to/from register
             if ((byte1 & 0xFF << 2) == 0b1000_1000)
             {
-                var w = (byte1 & 0b0000_0001) > 0;
+                var w = byte1 & 0b0000_0001;
 
                 var byte2 = bytes[index++];
-                var mod = (byte2 & MOD_MASK) >> 6;
-                var rm = byte2 & RM_MASK;
+                var mod = (byte2 & 0b1100_0000) >> 6;
 
-                var regTable = w ? RegTable_W1 : RegTable_W0;
-
-                var reg = (byte2 & REG_MASK) >> 3;
+                var rm = (byte2 & 0b0111) | (w << 3);
+                
+                //include w to correctly index into indexes 8 to 15 on RegTable
+                var reg = ((byte2 & 0b0011_1000) >> 3) | (w << 3);
                 //register to register
                 if (mod == 0b11)
                 {
-                    strBuilder.AppendLine($"mov {regTable[rm]}, {regTable[reg]}");
+                    strBuilder.AppendLine($"mov {RegTable[rm]}, {RegTable[reg]}");
                 }
                 else
                 {
@@ -107,19 +109,20 @@ public static class Decoder8086
                     var addressCalc = decodeEffectAddressCalculation(byte2);
 
                     strBuilder.AppendLine(d
-                        ? $"mov {regTable[reg]}, {addressCalc}"
-                        : $"mov {addressCalc}, {regTable[reg]}");
+                        ? $"mov {RegTable[reg]}, {addressCalc}"
+                        : $"mov {addressCalc}, {RegTable[reg]}");
                 }
             }
             //immediate to register
             else if ((byte1 & 0xFF << 4) == 0b1011_0000)
             {
-                var w = (byte1 & 0b0000_1000) > 0;
-                var reg = byte1 & 0b0000_0111;
+                var w = (byte1 & 0b0000_1000) >> 3;
+                
+                //include w to correctly index into indexes 8 to 15 on RegTable
+                var reg = (byte1 & 0b0000_0111) | (w << 3);
 
-                var immediate = w ? readWord() : readByte();
-                var regTable = w ? RegTable_W1 : RegTable_W0;
-                strBuilder.AppendLine($"mov {regTable[reg]}, {immediate}");
+                var immediate = w > 0 ? readWord() : readByte();
+                strBuilder.AppendLine($"mov {RegTable[reg]}, {immediate}");
             }
             //immediate to memory
             else if ((byte1 & 0xFF << 1) == 0b1100_0110)
--- computer-enhance/perfaware/listing-39-40/personaltest.asm
+++ computer-enhance/perfaware/listing-39-40/personaltest.asm
@@ -1,9 +1,3 @@
 bits 16
 
-; Memory-to-accumulator test
-mov ax, [2555]
-mov ax, [16]
-
-; Accumulator-to-memory test
-mov [2554], ax
-mov [15], ax
+mov dx, 3948
