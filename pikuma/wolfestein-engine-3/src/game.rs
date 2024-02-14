9f9f2cbee0ccb91d09e25bfdd99f0d6acb0c4c89
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-02-13 19:25:27 -0800

    Simple grid math

--- pikuma/wolfestein-engine-3/src/game.rs
+++ pikuma/wolfestein-engine-3/src/game.rs
@@ -5,12 +5,50 @@ use crate::graphics::Viewport2D;
 
 use self::level::{create_level, draw_mini_map};
 
-mod level {
-    use std::vec;
-
+mod grid {
+    use std::ops::Add;
     use macroquad::prelude::*;
 
+    pub struct GridCoords {
+        pub x: i32,
+        pub y: i32,
+    }
+
+    impl Add<GridCoords> for GridCoords{
+        type Output = GridCoords;
+
+        fn add(self, rhs: GridCoords) -> Self::Output {
+            GridCoords {
+                x: self.x + rhs.x,
+                y: self.y + rhs.y,
+            }
+        }
+    }
+
+    pub fn coords(x: i32, y: i32) -> GridCoords{
+        GridCoords { x, y }
+    }
+
+    pub fn tile_bl(x: i32, y: i32, tile_size: f32) -> Vec2{
+        vec2(x as f32 * tile_size, y as f32 * tile_size)
+    }
+
+    pub fn tile_center(x: i32, y:i32, tile_size: f32) -> Vec2 {
+        tile_bl(x, y, tile_size) + vec2(tile_size, tile_size) * 0.5
+    }
+
+    pub fn pos_to_coords(pos: Vec2, tile_size: f32) -> GridCoords{
+        let x = (pos.x /tile_size).floor() as i32;
+        let y = (pos.y /tile_size).floor() as i32;
+        coords(x, y)
+    }
+}
+
+mod level {
+    use macroquad::prelude::*;
     use crate::graphics::Viewport2D;
+
+    use super::grid;
 
     #[derive(PartialEq, Clone, Copy, Debug)]
     pub enum LevelBlock {
@@ -67,9 +105,7 @@ mod level {
         }
 
         pub fn tile_center(&self, x: i32, y: i32) -> Vec2 {
-            vec2(x as f32 * self.tile_size, y as f32 * self.tile_size)
-                + vec2(self.tile_size, self.tile_size) * 0.5
-                - self.extents()
+            grid::tile_center(x, y, self.tile_size) - self.extents()
         }
     }
 
