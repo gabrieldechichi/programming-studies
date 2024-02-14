8d4fe7a3df1f6678aeca9c53482b93521062e02d
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-02-13 19:56:51 -0800

    wolfestein-v3: player

--- pikuma/wolfestein-engine-3/src/game.rs
+++ pikuma/wolfestein-engine-3/src/game.rs
@@ -1,20 +1,17 @@
+use crate::*;
 use macroquad::color::Color;
 use macroquad::prelude::*;
 
-use crate::graphics::Viewport2D;
-
-use self::level::{create_level, draw_mini_map};
-
 mod grid {
-    use std::ops::Add;
     use macroquad::prelude::*;
+    use std::ops::Add;
 
     pub struct GridCoords {
         pub x: i32,
         pub y: i32,
     }
 
-    impl Add<GridCoords> for GridCoords{
+    impl Add<GridCoords> for GridCoords {
         type Output = GridCoords;
 
         fn add(self, rhs: GridCoords) -> Self::Output {
@@ -25,28 +22,28 @@ mod grid {
         }
     }
 
-    pub fn coords(x: i32, y: i32) -> GridCoords{
+    pub fn coords(x: i32, y: i32) -> GridCoords {
         GridCoords { x, y }
     }
 
-    pub fn tile_bl(x: i32, y: i32, tile_size: f32) -> Vec2{
+    pub fn tile_bl(x: i32, y: i32, tile_size: f32) -> Vec2 {
         vec2(x as f32 * tile_size, y as f32 * tile_size)
     }
 
-    pub fn tile_center(x: i32, y:i32, tile_size: f32) -> Vec2 {
+    pub fn tile_center(x: i32, y: i32, tile_size: f32) -> Vec2 {
         tile_bl(x, y, tile_size) + vec2(tile_size, tile_size) * 0.5
     }
 
-    pub fn pos_to_coords(pos: Vec2, tile_size: f32) -> GridCoords{
-        let x = (pos.x /tile_size).floor() as i32;
-        let y = (pos.y /tile_size).floor() as i32;
+    pub fn pos_to_coords(pos: Vec2, tile_size: f32) -> GridCoords {
+        let x = (pos.x / tile_size).floor() as i32;
+        let y = (pos.y / tile_size).floor() as i32;
         coords(x, y)
     }
 }
 
 mod level {
-    use macroquad::prelude::*;
     use crate::graphics::Viewport2D;
+    use macroquad::prelude::*;
 
     use super::grid;
 
@@ -152,25 +149,121 @@ mod level {
     }
 }
 
-pub async fn run() {
-    let level = create_level();
-    let clear_color = Color::from_hex(0x333333);
-    let map_viewport = Viewport2D::new(Vec2::ZERO, vec2(screen_width(), screen_height()), 1.);
+mod math {
+    use macroquad::prelude::*;
 
-    // let minimap_viewport = Viewport2D {
-    //     size: vec2(screen_width(), screen_height()),
-    //     pivot: -vec2(screen_width(), screen_height()) * 0.5 + vec2(screen_width(), screen_height()) * 0.5*0.4,
-    //     scale: 0.4,
-    // };
-    // map_viewport = minimap_viewport;
+    pub fn polar_to_cartesian(r: f32, theta_radians: f32) -> Vec2 {
+        vec2(theta_radians.cos() * r, theta_radians.sin() * r)
+    }
+}
+
+mod player {
+    use macroquad::{math, prelude::*};
+
+    use crate::graphics::Viewport2D;
+
+    pub struct PlayerBundle {
+        pub transform: PlayerTransform,
+        pub movement: PlayerMovement,
+        pub minimap_graphics: PlayerMinimapGraphics,
+    }
+
+    pub struct PlayerMovement {
+        pub v: f32,
+        pub w: f32,
+    }
+
+    pub struct PlayerTransform {
+        pub pos: Vec2,
+        pub rot: f32,
+    }
+
+    pub struct PlayerMinimapGraphics {
+        pub color: Color,
+        pub radius: f32,
+        pub arrow_length: f32,
+    }
+
+    pub fn create_player() -> PlayerBundle {
+        PlayerBundle {
+            transform: PlayerTransform {
+                pos: Vec2::ZERO,
+                rot: 0.0,
+            },
+            movement: PlayerMovement {
+                v: 60.0,
+                w: 90_f32.to_radians(),
+            },
+            minimap_graphics: PlayerMinimapGraphics {
+                color: PURPLE,
+                radius: 10.,
+                arrow_length: 20.,
+            },
+        }
+    }
+
+    pub fn update_player(player: &mut PlayerBundle, dt: f32) {
+        let mut rot_input = 0.;
+        if is_key_down(KeyCode::A) {
+            rot_input += 1.;
+        }
+        if is_key_down(KeyCode::D) {
+            rot_input -= 1.;
+        }
+
+        let mut move_input = 0.;
+        if is_key_down(KeyCode::W) {
+            move_input += 1.;
+        }
+        if is_key_down(KeyCode::S) {
+            move_input -= 1.;
+        }
+
+        let new_rot = player.transform.rot + rot_input * player.movement.w * dt;
+        let ds = math::polar_to_cartesian(move_input, new_rot) * player.movement.v * dt;
+
+        player.transform.pos += ds;
+        player.transform.rot = new_rot;
+    }
+
+    pub fn draw_player_minimap(player: &PlayerBundle, viewport: &Viewport2D) {
+        viewport.draw_line(
+            player.transform.pos,
+            player.transform.pos
+                + math::polar_to_cartesian(
+                    player.minimap_graphics.arrow_length,
+                    player.transform.rot,
+                ),
+            2.,
+            BLACK,
+        );
+        viewport.draw_circle(
+            player.transform.pos,
+            player.minimap_graphics.radius,
+            player.minimap_graphics.color,
+        );
+    }
+}
+
+pub async fn run() {
+    let level = level::create_level();
+    let mut player = player::create_player();
+    let clear_color = Color::from_hex(0x333333);
+    let map_viewport =
+        graphics::Viewport2D::new(Vec2::ZERO, vec2(screen_width(), screen_height()), 1.);
 
     loop {
-        let _dt = get_frame_time();
+        let dt = get_frame_time();
 
+        //simulate
+        {
+            player::update_player(&mut player, dt);
+        }
         //draw
         {
             clear_background(clear_color);
-            draw_mini_map(&map_viewport, &level);
+            level::draw_mini_map(&map_viewport, &level);
+            player::draw_player_minimap(&player, &map_viewport);
         }
         next_frame().await;
     }
