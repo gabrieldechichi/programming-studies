5d87062393ffc40877b6eddda01db320941fa37e
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-04-06 17:02:10 -0700

    Finish debug configuration

--- programming-languages/zig/lang_basics/.nvim-dap.lua
+++ programming-languages/zig/lang_basics/.nvim-dap.lua
@@ -1,18 +1,6 @@
-local codelldb_bin = require("mason-registry").get_package("codelldb"):get_install_path() ..
-    "/extension/adapter/codelldb.exe"
-local executable_path = "/zig-out/bin/lang_basics.exe"
 local build_cmd = "zig build"
 
 local dap = require("dap")
-
-dap.adapters.codelldb = {
-    type = 'server',
-    port = "${port}",
-    executable = {
-        command = codelldb_bin,
-        args = { "--port", "${port}" },
-    }
-}
 
 dap.configurations.zig = {
     {
@@ -20,19 +8,10 @@ dap.configurations.zig = {
         type = "codelldb",
         request = "launch",
         program = function()
-            vim.notify("Running " .. build_cmd, vim.log.levels.INFO)
-
-            local build_output = vim.fn.system(build_cmd)
-
-            if vim.v.shell_error == 0 then
-                vim.notify("Buld successful: " .. build_output, vim.log.levels.INFO)
-                return vim.fn.getcwd() .. executable_path
-            else
-                vim.notify("Build failed. Check the output for errors.", vim.log.levels.ERROR)
-                return ""
-            end
+            return _G.build_and_debug(build_cmd)
         end,
         cwd = "${workspaceFolder}",
         stopOnEntry = false,
+        args = {}
     }
 }
