87c3677717daac519f5f61365cbe2b288897219c
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-04-10 19:55:49 -0700

    zig guess number

--- programming-languages/zig/guess_number/src/main.zig
+++ programming-languages/zig/guess_number/src/main.zig
@@ -1,39 +1,39 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var v : ?bool = null;
-    var p = &v;
-    try std.io.getStdOut().writer().print("value: {?}, pointer {?}\n", .{p.*, p});
-    p.* = true;
-    try std.io.getStdOut().writer().print("value: {?}, pointer {?}\n", .{p.*, p});
-    
-    // const stdout = std.io.getStdOut().writer();
-    // var xoshiro = std.rand.DefaultPrng.init(1234);
-    // var rand = xoshiro.random();
-    // const random = getRandomNumber(&rand);
-    // try stdout.print("Guess the number (1-100): {d}", .{random});
-    //
-    // while (true){
-    //     const guess = try getGuessFromUser();
-    //     if (guess < random){
-    //         // try stdout.print("Too low! Try again");
-    //     } else if (guess > random) {
-    //         // try stdout.print("Too high! Try again");
-    //     } else {
-    //         // try stdout.print("You're right!");
-    //         break;
-    //     }
-    // }
+    const seed = @as(u64, @intCast(std.time.milliTimestamp()));
+    var xoshiro = std.rand.DefaultPrng.init(seed);
+    var rand = xoshiro.random();
+    const random = rand.uintLessThan(u8, 100) + 1;
+
+    try printLine("Guess the number (1-100):", .{});
+
+    while (true) {
+        const guess = try getGuessFromUser();
+        if (guess < random) {
+            try printLine("Too low! Try again", .{});
+        } else if (guess > random) {
+            try printLine("Too high! Try again", .{});
+        } else {
+            try printLine("You're right!", .{});
+            break;
+        }
+    }
 }
 
-fn getRandomNumber(random: *std.rand.Random) u8 {
-    return random.uintLessThan(u8, 100) + 1;
+fn printLine(comptime format: []const u8, args: anytype) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print(format ++ "\n", args);
 }
 
 fn getGuessFromUser() !u8 {
-    var buffer: [20]u8 = undefined;
-    _ = try std.io.getStdIn().reader().readUntilDelimiterOrEof(&buffer, '\n');
-    try std.io.getStdOut().writer().print("{s}", .{buffer});
-    return 72;
-    // return try std.fmt.parseInt(u8, &buffer, 10);
+    const in = std.io.getStdIn();
+    var br = std.io.bufferedReader(in.reader());
+    var reader = br.reader();
+    var msg_buffer: [20]u8 = undefined;
+    var msg = try reader.readUntilDelimiterOrEof(&msg_buffer, '\n');
+    if (msg) |m| {
+        return try std.fmt.parseInt(u8, m[0..m.len-1], 10);
+    }
+    return 0;
 }
