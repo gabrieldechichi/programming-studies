14420a0daecf71b98d93aa940da81269064571a5
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-04-16 20:13:06 -0700

    heaps.io: Initial commit

---
+++ programming-languages/haxe/heaps_samples/1_hello_world/src/App1_helloWorld.hx
@@ -0,0 +1,6 @@
+class App1_HelloWorld extends hxd.App {
+	override function init() {
+		var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+		tf.text = "Hello World !";
+	}
+}
---
+++ programming-languages/haxe/heaps_samples/1_hello_world/src/Main.hx
@@ -0,0 +1,7 @@
+import App1_helloWorld.App1_HelloWorld;
+
+class Main extends App1_HelloWorld {
+	static function main() {
+		new Main();
+	}
+}
