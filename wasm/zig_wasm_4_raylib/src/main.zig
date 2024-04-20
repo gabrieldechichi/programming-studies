e1176af4d9d89fa1c8d26e67bc82adc364538256
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-04-20 16:49:34 -0700

    zig + raylib + wasm: 10k balls

--- wasm/zig_wasm_4_raylib/src/main.zig
+++ wasm/zig_wasm_4_raylib/src/main.zig
@@ -8,7 +8,7 @@ const ScreenSize = struct {
     height: f32,
 };
 
-const Ball = struct { pos: Vector2, velocity: Vector2, radius: f32 };
+const Ball = struct { pos: Vector2, velocity: Vector2, radius: f32, color: rl.Color };
 
 fn sqred(n: anytype) @TypeOf(n) {
     return n * n;
@@ -17,24 +17,37 @@ fn sqred(n: anytype) @TypeOf(n) {
 const ScreenPos = struct { x: i32, y: i32 };
 
 pub fn main() !void {
+    const allocator = std.heap.c_allocator;
     rl.InitWindow(960, 540, "My Window Name");
-    rl.SetTargetFPS(60);
+    rl.SetTargetFPS(30);
     defer rl.CloseWindow();
 
-    var ball = Ball{ .radius = 20.0, .pos = Vector2{ .x = 0, .y = 0 }, .velocity = Vector2{ .x = 150, .y = -200 } };
+    var rnd = std.rand.DefaultPrng.init(0);
+
+    const total_balls = 10000;
+    var balls = try std.ArrayList(Ball).initCapacity(allocator, total_balls);
+    for (0..total_balls) |_| {
+        const color = rl.Color{ .r = rnd.random().uintLessThan(u8, 255), .g = rnd.random().uintLessThan(u8, 255), .b = rnd.random().uintLessThan(u8, 255), .a = 254 };
+        const vx: f32 = @floatFromInt(std.math.sign(rnd.random().int(u16)) * (rnd.random().uintAtMost(u16, 100) + 200));
+        const vy: f32 = @floatFromInt(std.math.sign(rnd.random().int(u16)) * (rnd.random().uintAtMost(u16, 100) + 200));
+        var ball = Ball{ .color = color, .radius = 20.0, .pos = Vector2{ .x = 0, .y = 0 }, .velocity = Vector2{ .x = vx, .y = vy } };
+        try balls.append(ball);
+    }
     var screen_size = ScreenSize{ .width = @floatFromInt(rl.GetScreenWidth()), .height = @floatFromInt(rl.GetScreenHeight()) };
 
     while (!rl.WindowShouldClose()) {
         //update
         {
             const dt = rl.GetFrameTime();
-            if (sqred(ball.pos.x) >= sqred((screen_size.width - ball.radius) / 2)) {
-                ball.velocity.x *= -1;
+            for (balls.items) |*ball| {
+                if (sqred(ball.pos.x) >= sqred((screen_size.width - ball.radius) / 2)) {
+                    ball.velocity.x *= -1;
+                }
+                if (sqred(ball.pos.y) >= sqred((screen_size.height - ball.radius) / 2)) {
+                    ball.velocity.y *= -1;
+                }
+                ball.pos = rlmath.Vector2Add(ball.pos, rlmath.Vector2Scale(ball.velocity, dt));
             }
-            if (sqred(ball.pos.y) >= sqred((screen_size.height - ball.radius) / 2)) {
-                ball.velocity.y *= -1;
-            }
-            ball.pos = rlmath.Vector2Add(ball.pos, rlmath.Vector2Scale(ball.velocity, dt));
         }
 
         //render
@@ -42,8 +55,13 @@ pub fn main() !void {
             rl.BeginDrawing();
             defer rl.EndDrawing();
             rl.ClearBackground(rl.BLACK);
-            var ball_screen_pos = ScreenPos{ .x = @intFromFloat(ball.pos.x + screen_size.width / 2), .y = @intFromFloat(ball.pos.y + screen_size.height / 2) };
-            rl.DrawCircle(ball_screen_pos.x, ball_screen_pos.y, ball.radius, rl.RED);
+
+            for (balls.items) |ball| {
+                var ball_screen_pos = ScreenPos{ .x = @intFromFloat(ball.pos.x + screen_size.width / 2), .y = @intFromFloat(ball.pos.y + screen_size.height / 2) };
+                rl.DrawCircle(ball_screen_pos.x, ball_screen_pos.y, ball.radius, ball.color);
+            }
+
+            rl.DrawFPS(12, 12);
         }
     }
 }
