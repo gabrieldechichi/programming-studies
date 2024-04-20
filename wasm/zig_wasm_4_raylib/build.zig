37217378ca11f6c293a5c2cdef62066efe6ca979
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-04-20 14:14:00 -0700

    Refactor build.zig

--- wasm/zig_wasm_4_raylib/build.zig
+++ wasm/zig_wasm_4_raylib/build.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 const raySdk = @import("raylib/src/build.zig");
-
-const builtin = @import("builtin");
-const emccOutputDir = "zig-out" ++ std.fs.path.sep_str ++ "htmlout" ++ std.fs.path.sep_str;
-const emccOutputFile = "index.html";
+const emcc = @import("emcc.zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -17,136 +14,26 @@ pub fn build(b: *std.Build) !void {
 }
 
 fn buildWeb(b: *std.Build, target: anytype, optimize: std.builtin.OptimizeMode) !void {
-    //START HACKS
-    // HACK: issue: https://github.com/ziglang/zig/issues/16776 is where the issue is submitted.
-    // Zig building to emscripten doesn't work, because the Zig standard library
-    // is missing some things in the C system. "std/c.zig" is missing fd_t,
-    // which causes compilation to fail. So build to wasi instead, until it gets
-    // fixed.
-    const new_target = std.zig.CrossTarget {
-        .cpu_arch = target.cpu_arch,
-        .cpu_model = target.cpu_model,
-        .cpu_features_add = target.cpu_features_add,
-        .cpu_features_sub = target.cpu_features_sub,
-        .os_tag = .wasi,
-        .os_version_min = target.os_version_min,
-        .os_version_max = target.os_version_max,
-        .glibc_version = target.glibc_version,
-        .abi = target.abi,
-        .dynamic_linker = target.dynamic_linker,
-        .ofmt = target.ofmt,
-    };
+    try emcc.resolveEmscriptenSysRoot(b);
 
-    //HACK: Related to 16776. when the updateTargetForWeb workaround gets removed, see if those are nessesary anymore
-    const webhack_c =
-        \\// Zig adds '__stack_chk_guard', '__stack_chk_fail', and 'errno',
-        \\// which emscripten doesn't actually support.
-        \\// Seems that zig ignores disabling stack checking,
-        \\// and I honestly don't know why emscripten doesn't have errno.
-        \\#include <stdint.h>
-        \\uintptr_t __stack_chk_guard;
-        \\//I'm not certain if this means buffer overflows won't be detected,
-        \\// However, zig is pretty safe from those, so don't worry about it too much.
-        \\void __stack_chk_fail(void){}
-        \\int errno;
-    ;
-
-    const webhack_c_file_step = b.addWriteFiles();
-    const web_hack_c_file = webhack_c_file_step.add("webhack.c", webhack_c);
-    //END HACKS
-
-    //try resolve emsdk path if not set
-    if (b.sysroot == null) {
-        const emsdk_path = try std.process.getEnvVarOwned(b.allocator, "EMSDK");
-        b.sysroot = try std.fs.path.join(b.allocator, &.{ emsdk_path, "/upstream/emscripten" });
-    }
-
-    // const emcc_run_arg = try std.fs.path.join(b.allocator, &.{ b.sysroot.?, "emcc.bat" });
-    const emcc_run_arg = try getEmccCmd(b);
-    defer b.allocator.free(emcc_run_arg);
-
-    var raylib_lib = try raySdk.addRaylib(b, target, optimize, .{});
-
-    const exe_lib = b.addStaticLibrary(.{
-        .name = "game",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = new_target,
-        .optimize = optimize,
-    });
+    //compile targets
+    var exe_lib = try emcc.compileForEmscripten(b, "game", "src/main.zig", target, optimize);
     exe_lib.addIncludePath(.{ .path = "raylib/src" });
-    exe_lib.addCSourceFile(.{ .file = web_hack_c_file, .flags = &[_][]u8{} });
-    exe_lib.step.dependOn(&webhack_c_file_step.step);
-
+    var raylib_lib = try raySdk.addRaylib(b, target, optimize, .{});
     exe_lib.linkLibrary(raylib_lib);
 
-    b.installArtifact(exe_lib);
-    b.installArtifact(raylib_lib);
+    //linking
+    var link_step = try emcc.linkForEmscripten(b, &[_]*std.build.CompileStep{ exe_lib, raylib_lib }, optimize);
 
-    var cwd = std.fs.cwd();
-    try cwd.makePath("./zig-out/htmlout");
+    //build step
+    const build_step = b.step("build", "Build game");
+    build_step.dependOn(&link_step.step);
 
-    const link_step = b.addSystemCommand(&[_][]const u8{emcc_run_arg});
-    link_step.addFileArg(exe_lib.getEmittedBin());
-    link_step.step.dependOn(&exe_lib.step);
-    link_step.addFileArg(raylib_lib.getEmittedBin());
-    link_step.step.dependOn(&raylib_lib.step);
-
-    link_step.addArgs(&[_][]const u8{
-        "-o",
-        emccOutputDir ++ emccOutputFile,
-        "-sFULL-ES3=1",
-        "-sUSE_GLFW=3",
-        "-sASYNCIFY",
-        "-O0",
-        "--emrun",
-    });
-
-    // const run_step = try emscriptenRunStep(b);
-    // run_step.step.dependOn(&emcc_command.step);
-    const run_option = b.step("game", "Game");
-    run_option.dependOn(&link_step.step);
-}
-
-pub fn getEmrunCmd(b: *std.Build) ![]u8 {
-    if (b.sysroot == null) {
-        const emsdk_path = try std.process.getEnvVarOwned(b.allocator, "EMSDK");
-        b.sysroot = try std.fs.path.join(b.allocator, &.{ emsdk_path, "/upstream/emscripten" });
-    }
-    if (b.sysroot == null) {
-        @panic("Pass '--sysroot \"[path to emsdk installation]/upstream/emscripten\"'");
-    }
-    // If compiling on windows , use emrun.bat.
-    const emrunExe = switch (builtin.os.tag) {
-        .windows => "emrun.bat",
-        else => "emrun",
-    };
-
-    return try std.fs.path.join(b.allocator, &.{ b.sysroot.?, emrunExe });
-}
-
-pub fn getEmccCmd(b: *std.Build) ![]u8 {
-    if (b.sysroot == null) {
-        const emsdk_path = try std.process.getEnvVarOwned(b.allocator, "EMSDK");
-        b.sysroot = try std.fs.path.join(b.allocator, &.{ emsdk_path, "/upstream/emscripten" });
-    }
-    if (b.sysroot == null) {
-        @panic("Pass '--sysroot \"[path to emsdk installation]/upstream/emscripten\"'");
-    }
-    // If compiling on windows , use emrun.bat.
-    const emrunExe = switch (builtin.os.tag) {
-        .windows => "emcc.bat",
-        else => "emcc",
-    };
-
-    return try std.fs.path.join(b.allocator, &.{ b.sysroot.?, emrunExe });
-}
-
-pub fn emscriptenRunStep(b: *std.Build) !*std.Build.Step.Run {
-    const emcc_run_arg = try getEmrunCmd(b);
-    defer b.allocator.free(emcc_run_arg);
-
-    const run_cmd = b.addSystemCommand(&[_][]const u8{ emcc_run_arg, emccOutputDir ++ emccOutputFile });
-    return run_cmd;
+    //run step
+    const run_step = b.step("run", "Run game");
+    const emscripten_run_step = try emcc.emscriptenRunStep(b);
+    emscripten_run_step.step.dependOn(&link_step.step);
+    run_step.dependOn(&emscripten_run_step.step);
 }
 
 fn buildNative(b: *std.Build, target: anytype, optimize: std.builtin.OptimizeMode) !void {
--- wasm/zig_wasm_4_raylib/emcc.zig
+++ wasm/zig_wasm_4_raylib/emcc.zig
@@ -1,136 +1,16 @@
-// raylib-zig (c) Nikolas Wipper 2020-2024
-
 const std = @import("std");
 const builtin = @import("builtin");
-
 const emccOutputDir = "zig-out" ++ std.fs.path.sep_str ++ "htmlout" ++ std.fs.path.sep_str;
 const emccOutputFile = "index.html";
-pub fn emscriptenRunStep(b: *std.Build) !*std.Build.Step.Run {
-    // Find emrun.
-    if (b.sysroot == null) {
-        @panic("Pass '--sysroot \"[path to emsdk installation]/upstream/emscripten\"'");
-    }
-    // If compiling on windows , use emrun.bat.
-    const emrunExe = switch (builtin.os.tag) {
-        .windows => "emrun.bat",
-        else => "emrun",
-    };
-    const emrun_run_arg = try b.allocator.alloc(u8, b.sysroot.?.len + emrunExe.len + 1);
-    defer b.allocator.free(emrun_run_arg);
 
-    _ = try std.fmt.bufPrint(emrun_run_arg, "{s}" ++ std.fs.path.sep_str ++ "{s}", .{ b.sysroot.?, emrunExe });
-
-    const run_cmd = b.addSystemCommand(&[_][]const u8{ emrun_run_arg, emccOutputDir ++ emccOutputFile });
-    return run_cmd;
-}
-
-// Creates the static library to build a project for Emscripten.
-pub fn compileForEmscripten(
-    b: *std.Build,
-    name: []const u8,
-    root_source_file: []const u8,
-    target: std.zig.CrossTarget,
-    optimize: std.builtin.Mode,
-) *std.Build.Step.Compile {
-    // TODO: It might be a good idea to create a custom compile step, that does
-    // both the compile to static library and the link with emcc by overidding
-    // the make function of the step. However it might also be a bad idea since
-    // it messes with the build system itself.
-
-    const new_target = updateTargetForWeb(target);
-
-    // The project is built as a library and linked later.
-    const exe_lib = b.addStaticLibrary(.{
-        .name = name,
-        .root_source_file = .{ .path = root_source_file },
-        .target = new_target,
-        .optimize = optimize,
-    });
-
-    // There are some symbols that need to be defined in C.
-    const webhack_c_file_step = b.addWriteFiles();
-    const webhack_c_file = webhack_c_file_step.add("webhack.c", webhack_c);
-    exe_lib.addCSourceFile(.{ .file = webhack_c_file, .flags = &[_][]u8{} });
-    // Since it's creating a static library, the symbols raylib uses to webgl
-    // and glfw don't need to be linked by emscripten yet.
-    exe_lib.step.dependOn(&webhack_c_file_step.step);
-    return exe_lib;
-}
-
-// Links a set of items together using emscripten.
-//
-// Will accept objects and static libraries as items to link. As for files to
-// include, it is recomended to have a single resources directory and just pass
-// the entire directory instead of passing every file individually. The entire
-// path given will be the path to read the file within the program. So, if
-// "resources/image.png" is passed, your program will use "resources/image.png"
-// as the path to load the file.
-//
-// TODO: Test if shared libraries are accepted, I don't remember if emcc can
-//       link a shared library with a project or not.
-// TODO: Add a parameter that allows a custom output directory.
-pub fn linkWithEmscripten(
-    b: *std.Build,
-    itemsToLink: []const *std.Build.Step.Compile,
-) !*std.Build.Step.Run {
-    // Raylib uses --sysroot in order to find emscripten, so do the same here.
-    if (b.sysroot == null) {
-        @panic("Pass '--sysroot \"[path to emsdk installation]/upstream/emscripten\"'");
-    }
-    const emccExe = switch (builtin.os.tag) {
-        .windows => "emcc.bat",
-        else => "emcc",
-    };
-    var emcc_run_arg = try b.allocator.alloc(u8, b.sysroot.?.len + emccExe.len + 1);
-    defer b.allocator.free(emcc_run_arg);
-
-    emcc_run_arg = try std.fmt.bufPrint(
-        emcc_run_arg,
-        "{s}" ++ std.fs.path.sep_str ++ "{s}",
-        .{ b.sysroot.?, emccExe },
-    );
-
-    // Create the output directory because emcc can't do it.
-    const mkdir_command = b.addSystemCommand(&[_][]const u8{ "Powershell.exe", "-Command", "New-Item", " -ItemType Directory", "-Force", "-Path", emccOutputDir });
-
-    // Actually link everything together.
-    const emcc_command = b.addSystemCommand(&[_][]const u8{emcc_run_arg});
-
-    for (itemsToLink) |item| {
-        emcc_command.addFileArg(item.getEmittedBin());
-        emcc_command.link_step.dependOn(&item.step);
-    }
-    // This puts the file in zig-out/htmlout/index.html.
-    emcc_command.link_step.dependOn(&mkdir_command.link_step);
-    emcc_command.addArgs(&[_][]const u8{
-        "-o",
-        emccOutputDir ++ emccOutputFile,
-        "-sFULL-ES3=1",
-        "-sUSE_GLFW=3",
-        "-sASYNCIFY",
-        "-O3",
-        "--emrun",
-    });
-    return emcc_command;
-}
-
-// TODO: See if zig's standard library already has somehing like this.
-fn lastIndexOf(string: []const u8, character: u8) usize {
-    // Interestingly, Zig has no nice way of iterating a slice backwards.
-    for (0..string.len) |i| {
-        const index = string.len - i - 1;
-        if (string[index] == character) return index;
-    }
-    return string.len - 1;
-}
-// TODO: each zig update, remove this and see if everything still works.
-// https://github.com/ziglang/zig/issues/16776 is where the issue is submitted.
-fn updateTargetForWeb(target: std.zig.CrossTarget) std.zig.CrossTarget {
+pub fn compileForEmscripten(b: *std.Build, name: []const u8, root_source_file: []const u8, target: anytype, optimize: std.builtin.OptimizeMode) !*std.build.Step.Compile {
+    //START HACKS
+    // HACK: issue: https://github.com/ziglang/zig/issues/16776 is where the issue is submitted.
     // Zig building to emscripten doesn't work, because the Zig standard library
     // is missing some things in the C system. "std/c.zig" is missing fd_t,
     // which causes compilation to fail. So build to wasi instead, until it gets
     // fixed.
-    return std.zig.CrossTarget{
+    const new_target = std.zig.CrossTarget{
         .cpu_arch = target.cpu_arch,
         .cpu_model = target.cpu_model,
         .cpu_features_add = target.cpu_features_add,
@@ -143,17 +23,94 @@ fn updateTargetForWeb(target: std.zig.CrossTarget) std.zig.CrossTarget {
         .dynamic_linker = target.dynamic_linker,
         .ofmt = target.ofmt,
     };
+
+    //HACK: Related to 16776. when the updateTargetForWeb workaround gets removed, see if those are nessesary anymore
+    const webhack_c =
+        \\// Zig adds '__stack_chk_guard', '__stack_chk_fail', and 'errno',
+        \\// which emscripten doesn't actually support.
+        \\// Seems that zig ignores disabling stack checking,
+        \\// and I honestly don't know why emscripten doesn't have errno.
+        \\#include <stdint.h>
+        \\uintptr_t __stack_chk_guard;
+        \\//I'm not certain if this means buffer overflows won't be detected,
+        \\// However, zig is pretty safe from those, so don't worry about it too much.
+        \\void __stack_chk_fail(void){}
+        \\int errno;
+    ;
+
+    const webhack_c_file_step = b.addWriteFiles();
+    const web_hack_c_file = webhack_c_file_step.add("webhack.c", webhack_c);
+    //END HACKS
+
+    const exe_lib = b.addStaticLibrary(.{
+        .name = name,
+        .root_source_file = .{ .path = root_source_file },
+        .target = new_target,
+        .optimize = optimize,
+    });
+    exe_lib.addCSourceFile(.{ .file = web_hack_c_file, .flags = &[_][]u8{} });
+    exe_lib.step.dependOn(&webhack_c_file_step.step);
+    return exe_lib;
 }
-const webhack_c =
-    \\// Zig adds '__stack_chk_guard', '__stack_chk_fail', and 'errno',
-    \\// which emscripten doesn't actually support.
-    \\// Seems that zig ignores disabling stack checking,
-    \\// and I honestly don't know why emscripten doesn't have errno.
-    \\// TODO: when the updateTargetForWeb workaround gets removed, see if those are nessesary anymore
-    \\#include <stdint.h>
-    \\uintptr_t __stack_chk_guard;
-    \\//I'm not certain if this means buffer overflows won't be detected,
-    \\// However, zig is pretty safe from those, so don't worry about it too much.
-    \\void __stack_chk_fail(void){}
-    \\int errno;
-;
+
+pub fn linkForEmscripten(b: *std.Build, itemsToLink: []const *std.Build.Step.Compile, optimize: std.builtin.OptimizeMode) !*std.build.Step.Run {
+    const emcc_run_arg = try getEmscriptenCmd(b, "emcc");
+    defer b.allocator.free(emcc_run_arg);
+
+    const link_step = b.addSystemCommand(&[_][]const u8{emcc_run_arg});
+
+    for (itemsToLink) |item| {
+        b.installArtifact(item);
+        link_step.addFileArg(item.getEmittedBin());
+        link_step.step.dependOn(&item.step);
+    }
+
+    var cwd = std.fs.cwd();
+    try cwd.makePath(emccOutputDir);
+
+    const opt_flag = switch (optimize) {
+        .Debug => "-O0",
+        .ReleaseSafe => "-O0",
+        .ReleaseSmall => "-Oz",
+        .ReleaseFast => "-O3",
+    };
+
+    link_step.addArgs(&[_][]const u8{
+        "-o",
+        emccOutputDir ++ emccOutputFile,
+        "-sFULL-ES3=1",
+        "-sUSE_GLFW=3",
+        "-sASYNCIFY",
+        opt_flag,
+        "--emrun",
+    });
+    return link_step;
+}
+
+pub fn resolveEmscriptenSysRoot(b: *std.Build) !void {
+    if (b.sysroot == null) {
+        const emsdk_path = try std.process.getEnvVarOwned(b.allocator, "EMSDK");
+        b.sysroot = try std.fs.path.join(b.allocator, &.{ emsdk_path, "/upstream/emscripten" });
+    }
+}
+
+pub fn getEmscriptenCmd(b: *std.Build, cmd: []const u8) ![]u8 {
+    if (b.sysroot == null) {
+        @panic("Pass '--sysroot \"[path to emsdk installation]/upstream/emscripten\"'");
+    }
+
+    if (builtin.os.tag == .windows) {
+        const cmd_bat = try std.fmt.allocPrint(b.allocator, "{s}.bat", .{cmd});
+        defer b.allocator.free(cmd_bat);
+        return try std.fs.path.join(b.allocator, &.{ b.sysroot.?, cmd_bat });
+    }
+    return try std.fs.path.join(b.allocator, &.{ b.sysroot.?, cmd });
+}
+
+pub fn emscriptenRunStep(b: *std.Build) !*std.Build.Step.Run {
+    const emcc_run_arg = try getEmscriptenCmd(b, "emrun");
+    defer b.allocator.free(emcc_run_arg);
+
+    const run_cmd = b.addSystemCommand(&[_][]const u8{ emcc_run_arg, emccOutputDir ++ emccOutputFile });
+    return run_cmd;
+}
