9d2a868be76e1f7d32550529b1c49f5639835db4
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-04-20 20:27:25 -0700

    draw frame time

--- wasm/zig_wasm_4_raylib/src/main.zig
+++ wasm/zig_wasm_4_raylib/src/main.zig
@@ -4,15 +4,18 @@ const rlmath = @cImport(@cInclude("raymath.h"));
 const Vector2 = rlmath.Vector2;
 
 const ScreenSize = struct {
-    width: f32,
-    height: f32,
+    width: u16,
+    height: u16,
+
+    pub fn width_f(self: *ScreenSize) f32 {
+        return @floatFromInt(self.width);
+    }
+    pub fn height_f(self: *ScreenSize) f32 {
+        return @floatFromInt(self.height);
+    }
 };
 
 const Ball = struct { pos: Vector2, velocity: Vector2, radius: f32, color: rl.Color };
-
-fn sqred(n: anytype) @TypeOf(n) {
-    return n * n;
-}
 
 const ScreenPos = struct { x: i32, y: i32 };
 
@@ -22,9 +25,14 @@ pub fn main() !void {
     rl.SetTargetFPS(30);
     defer rl.CloseWindow();
 
+    var perf_stats_buffer: [11]u8 = undefined;
+
+    var screen_size = ScreenSize{ .width = @intCast(rl.GetScreenWidth()), .height = @intCast(rl.GetScreenHeight()) };
+
     var rnd = std.rand.DefaultPrng.init(0);
 
-    const total_balls = 10000;
+    const batch_size = 8;
+    const total_balls = batch_size * 1024;
     var balls = try std.ArrayList(Ball).initCapacity(allocator, total_balls);
     for (0..total_balls) |_| {
         const color = rl.Color{ .r = rnd.random().uintLessThan(u8, 255), .g = rnd.random().uintLessThan(u8, 255), .b = rnd.random().uintLessThan(u8, 255), .a = 254 };
@@ -33,17 +41,16 @@ pub fn main() !void {
         var ball = Ball{ .color = color, .radius = 20.0, .pos = Vector2{ .x = 0, .y = 0 }, .velocity = Vector2{ .x = vx, .y = vy } };
         try balls.append(ball);
     }
-    var screen_size = ScreenSize{ .width = @floatFromInt(rl.GetScreenWidth()), .height = @floatFromInt(rl.GetScreenHeight()) };
 
     while (!rl.WindowShouldClose()) {
         //update
         {
             const dt = rl.GetFrameTime();
             for (balls.items) |*ball| {
-                if (sqred(ball.pos.x) >= sqred((screen_size.width - ball.radius) / 2)) {
+                if (ball.pos.x - ball.radius < -screen_size.width_f() / 2 or ball.pos.x + ball.radius > screen_size.width_f() / 2) {
                     ball.velocity.x *= -1;
                 }
-                if (sqred(ball.pos.y) >= sqred((screen_size.height - ball.radius) / 2)) {
+                if (ball.pos.y - ball.radius < -screen_size.height_f() / 2 or ball.pos.y + ball.radius > screen_size.height_f() / 2) {
                     ball.velocity.y *= -1;
                 }
                 ball.pos = rlmath.Vector2Add(ball.pos, rlmath.Vector2Scale(ball.velocity, dt));
@@ -53,15 +60,20 @@ pub fn main() !void {
         //render
         {
             rl.BeginDrawing();
-            defer rl.EndDrawing();
-            rl.ClearBackground(rl.BLACK);
 
-            for (balls.items) |ball| {
-                var ball_screen_pos = ScreenPos{ .x = @intFromFloat(ball.pos.x + screen_size.width / 2), .y = @intFromFloat(ball.pos.y + screen_size.height / 2) };
-                rl.DrawCircle(ball_screen_pos.x, ball_screen_pos.y, ball.radius, ball.color);
+            {
+                rl.ClearBackground(rl.BLACK);
+                for (balls.items) |ball| {
+                    var ball_screen_pos = ScreenPos{ .x = @intFromFloat(ball.pos.x + screen_size.width_f() / 2), .y = @intFromFloat(ball.pos.y + screen_size.height_f() / 2) };
+                    rl.DrawCircle(ball_screen_pos.x, ball_screen_pos.y, ball.radius, ball.color);
+                }
+                const stats = try std.fmt.bufPrint(&perf_stats_buffer, "Dt: {d:.2}ms", .{rl.GetFrameTime() * 1000});
+                rl.DrawText(@as([*c]const u8, @ptrCast(stats)), 12, 12, 20, rl.ORANGE);
+
+                rl.EndTextureMode();
             }
 
-            rl.DrawFPS(12, 12);
+            rl.EndDrawing();
         }
     }
 }
