36e31eeb6562c2dd815cc66002681b093b2a3b48
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-05-04 09:03:36 -0700

    makefile: simple makefile

---
+++ programming-languages/c/make-tut/hello.h
@@ -0,0 +1,10 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+
+static void hello(void){
+    printf("Hello World\n");
+}
+
+#endif // HELLO_H
---
+++ programming-languages/c/make-tut/main.c
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include <stdio.h>
+
+#define ADD(a,b) a + b
+
+int main() {
+    int sum = ADD(1, 2);
+    printf("%d\n\n", sum);
+    hello();
+}
