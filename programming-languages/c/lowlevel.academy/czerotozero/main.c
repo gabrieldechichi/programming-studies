a011c8f4e0820ac7826c5e700cd2284edf92c924
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-05-08 19:48:35 -0700

    czerotozero: structs

--- programming-languages/c/lowlevel.academy/czerotozero/main.c
+++ programming-languages/c/lowlevel.academy/czerotozero/main.c
@@ -1,12 +1,19 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    int id;
+    char firstname[64];
+    char lastname[64];
+    float income;
+    bool ismanager;
+} employee_t;
+
 int main() {
-    char *str = "hehe";
-    char other_str[] = {'h', 'e', 'h', 'e', 0};
-    if (!strcmp(str, other_str)){
-        printf("Yay!\n");
-    } else{
-        printf("Nay!\n");
-    }
+    employee_t fred = {};
+    fred.id = 0;
+    fred.ismanager = true;
+    strcpy(fred.firstname, "Fred");
+    printf("Employee name: %s (%d)\n", fred.firstname, (int) strlen(fred.firstname));
 }
