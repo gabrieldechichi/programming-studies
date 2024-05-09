224426ff9e3efb06beda5045f8e5ce619317314e
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-05-08 20:15:06 -0700

    czerotozero: pointers

--- programming-languages/c/lowlevel.academy/czerotozero/main.c
+++ programming-languages/c/lowlevel.academy/czerotozero/main.c
@@ -9,4 +9,10 @@ typedef struct {
     bool ismanager;
 } __attribute((__packed__)) employee_t;
 
-int main() { printf("Size of struct %d \n", (int)sizeof(employee_t)); }
+int main() {
+    int x = 2;
+    int y = 2;
+    bool b = true;
+    printf("&x: %p\n&y: %p\n&b: %p\n", &x, &y, &b);
+    printf("&x - &y: %ld\n", (long)((char *)&y - (char *)&x));
+}
