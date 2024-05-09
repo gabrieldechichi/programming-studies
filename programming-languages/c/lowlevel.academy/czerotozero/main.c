2fcf80d2927458dcc6498915183079cd7ffe462e
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-05-08 21:05:47 -0700

    czerotozero: file write

--- programming-languages/c/lowlevel.academy/czerotozero/main.c
+++ programming-languages/c/lowlevel.academy/czerotozero/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main () {
-    FILE* file = fopen("srdkdj.txt", "r");
-    if (file == NULL){
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return -1;
+    }
+
+    char *file_name = argv[1];
+
+    FILE *file = fopen(file_name, "w+");
+    if (file == NULL) {
         perror("Error opening file");
         return -1;
     }
+
+    char *buffer = "Hello new file!";
+    fwrite(buffer, sizeof(*buffer), strlen(buffer), file);
+
     fclose(file);
     return 0;
 }
