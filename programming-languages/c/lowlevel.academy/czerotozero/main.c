1d86abacb3c0c292507726543e94c8f6aa05e2f1
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-05-08 20:52:59 -0700

    czerotozero: perrr

--- programming-languages/c/lowlevel.academy/czerotozero/main.c
+++ programming-languages/c/lowlevel.academy/czerotozero/main.c
@@ -1,48 +1,11 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct {
-    int id;
-    char firstname[64];
-    char lastname[64];
-    float income;
-    bool ismanager;
-} __attribute((__packed__)) employee_t;
-
-void init_employee(employee_t *employee) {
-    static int id_count = 0;
-    memset(employee, 0, sizeof(employee_t));
-    employee->id = id_count++;
-    sprintf(employee->firstname, "Employee %d", employee->id);
-    employee->ismanager = false;
-}
-
-void print_employee(employee_t *employee) {
-    if (employee) {
-        printf("ID: %d\nName: %s %s (%d)\n", employee->id, employee->firstname,
-               employee->lastname,
-               (int) (strlen(employee->firstname) + strlen(employee->lastname)));
-    } else {
-        printf("Null employee");
-    }
-}
-
-int main() {
-    // comes from db header
-    int n = 4;
-
-    employee_t *employees = malloc(sizeof(employee_t) * n);
-    if (employees == NULL) {
-        printf("Failed allocating employees. %s:%d\n", __FILE__, __LINE__);
+int main () {
+    FILE* file = fopen("srdkdj.txt", "r");
+    if (file == NULL){
+        perror("Error opening file");
         return -1;
     }
-    for (int i = 0; i < n; i++) {
-        init_employee(&employees[i]);
-        print_employee(&employees[i]);
-    }
-
-    free(employees);
-    employees = NULL;
+    fclose(file);
+    return 0;
 }
