55e7075261fe5d06d1b614f6a9dd146d8a1d306d
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-05-08 21:32:52 -0700

    czerotozero: read and write db header

--- programming-languages/c/lowlevel.academy/czerotozero/main.c
+++ programming-languages/c/lowlevel.academy/czerotozero/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+typedef unsigned short ushort;
+typedef unsigned int uint;
+
+typedef struct {
+    ushort version;
+    ushort employees;
+    uint filesize;
+} db_header_t;
+
+int write_sample_header(char *file_name) {
+    db_header_t header = {0};
+    header.version = 1;
+    header.employees = 65;
+    header.filesize = sizeof(header);
+
+    FILE *file = fopen(file_name, "w+");
+    if (file == NULL) {
+        perror("write_sample_header, fopen");
+        return -1;
+    }
+
+    size_t size = fwrite(&header, sizeof(header), 1, file);
+    if (size != 1){
+        perror("Error writing to file");
+        return -1;
+    }
+    fclose(file);
+    return 0;
+}
+
+int read_header(char *file_name) {
+    FILE *file = fopen(file_name, "r");
+    if (file == NULL) {
+        perror("read_header");
+        return -1;
+    }
+    db_header_t header = {0};
+    size_t size = fread(&header, sizeof(db_header_t), 1, file);
+    if (size != 1) {
+        perror("Error reading file");
+        return -1;
+    }
+
+    printf("version: %d\nemployes: %d\nfilesize: %d\n", header.version,
+           header.employees, header.filesize);
+    fclose(file);
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -10,16 +57,8 @@ int main(int argc, char *argv[]) {
     }
 
     char *file_name = argv[1];
+    write_sample_header(file_name);
 
-    FILE *file = fopen(file_name, "w+");
-    if (file == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
-
-    char *buffer = "Hello new file!";
-    fwrite(buffer, sizeof(*buffer), strlen(buffer), file);
-
-    fclose(file);
+    read_header(file_name);
     return 0;
 }
