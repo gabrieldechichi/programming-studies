0331a3efa204ef645a28e183454074eb6f9ca1ac
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-06-17 15:37:07 -0700

    gui_task_management: macro hell!

--- programming-languages/c/gui_task_management/main.c
+++ programming-languages/c/gui_task_management/main.c
@@ -8,6 +8,15 @@
 #define HEIGHT 720
 #define WINMARGIN 2
 
+#define concat(a, b) a##b
+#define macro_var(name) concat(name, __LINE__)
+#define defer(start, end)                                                      \
+    for (int macro_var(_i_) = ((start), 0); !macro_var(_i_);                     \
+         (macro_var(_i_) += 1), (end))
+
+#define lf_scope defer(lf_begin(), lf_end())
+#define lf_div_scope(start_args) defer(lf_div_begin start_args, lf_div_end)
+
 int main() {
     glfwInit();
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Todo", NULL, NULL);
@@ -25,23 +34,23 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
-        lf_begin();
-        lf_div_begin(
-            ((vec2s){WINMARGIN, WINMARGIN}),
-            ((vec2s){WIDTH - WINMARGIN * 2.0f, HEIGHT - WINMARGIN * 2}), true);
-
-        lf_push_font(&titlefont);
-        lf_text("Your TODOS");
-        lf_pop_font();
-
-        lf_div_end();
-        lf_end();
+        lf_scope {
+            lf_div_scope(
+                (((vec2s){WINMARGIN, WINMARGIN}),
+                 ((vec2s){WIDTH - WINMARGIN * 2.0f, HEIGHT - WINMARGIN * 2}),
+                 true)) {
+                lf_push_font(&titlefont);
+                lf_text("Your TODOS");
+                lf_pop_font();
+            }
+        }
 
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
 
     lf_terminate();
+    lf_free_font(&titlefont);
     glfwDestroyWindow(window);
     glfwTerminate();
 }
