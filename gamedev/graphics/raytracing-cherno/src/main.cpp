497541b6e3636d96a677e229fadbd33710c47e26
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-07-17 19:20:14 -0700

    raytracing-cherno: diffuse lighting

--- gamedev/graphics/raytracing-cherno/src/main.cpp
+++ gamedev/graphics/raytracing-cherno/src/main.cpp
@@ -6,6 +6,7 @@
 #include "texture.hpp"
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
 
@@ -48,15 +49,12 @@ class Renderer {
 };
 
 glm::vec4 Renderer::frag(glm::vec2 coord) {
-    // a =(bx^2 + by^2 + bz ^ 2)
-    // b = 2 * (ax*bx + ay*by + az * bz)
-    // c = (ax^2 + ay^2 + az^2 - r^2)
-    // a*t^2 + bt + c = 0
-    //
+    glm::vec3 lightDir(-1,-1,1);
+    lightDir = glm::normalize(lightDir);
 
-    glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
-    glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
-    rayDirection = glm::normalize(rayDirection);
+    glm::vec3 rayOrigin(0.0f, 0.0f, -2.0f);
+    glm::vec3 rayDirection(coord.x, coord.y, 1.0f);
+    //rayDirection = glm::normalize(rayDirection);
     float radius = 0.5f;
 
     float a = glm::dot(rayDirection, rayDirection);
@@ -64,10 +62,24 @@ glm::vec4 Renderer::frag(glm::vec2 coord) {
     float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
 
     float discriminant = b * b - 4.0f * a * c;
-    if (discriminant >= 0.0f)
-        return glm::vec4(1, 0, 1, 1);
+    if (discriminant < 0.0f) {
+        return glm::vec4(0.2, 0.2, 0.2, 1);
+    }
 
-    return glm::vec4(0, 0, 0, 1);
+    float discriminantSqroot = std::sqrt(discriminant);
+    float t1 = (-b + discriminantSqroot) / (2 * a);
+    float t2 = (-b - discriminantSqroot) / (2 * a);
+
+    float t = t1 < t2 ? t1 : t2;
+
+    auto hitPoint = rayOrigin + rayDirection * t;
+    auto normal = glm::normalize(hitPoint);
+    // normal = normal * 0.5f + 0.5f;
+    auto diffuse = glm::dot(normal, -lightDir);
+    auto color = glm::vec3(1,1,1);
+    color *= diffuse;
+
+    return glm::vec4(color, 1);
 }
 
 void Renderer::render() {
