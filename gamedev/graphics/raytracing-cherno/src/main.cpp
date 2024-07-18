1e07ef47cdda6ef8b6c38f4f2a423a20dcd66260
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-07-17 18:41:14 -0700

    raytracing-cherno: sphere

--- gamedev/graphics/raytracing-cherno/src/main.cpp
+++ gamedev/graphics/raytracing-cherno/src/main.cpp
@@ -1,4 +1,6 @@
-#include "glm/fwd.hpp"
+#include "glm/common.hpp"
+#include "glm/geometric.hpp"
+#include "glm/glm.hpp"
 #include "imgui.h"
 #include "platform.hpp"
 #include "texture.hpp"
@@ -18,10 +20,10 @@ struct Pixel {
     Pixel() = default;
 
     explicit Pixel(const glm::vec4 &v)
-        : r(static_cast<unsigned char>(v.r * 255.0f)),
-          g(static_cast<unsigned char>(v.g * 255.0f)),
-          b(static_cast<unsigned char>(v.b * 255.0f)),
-          a(static_cast<unsigned char>(v.a * 255.0f)) {}
+        : r(static_cast<unsigned char>(std::fmax(0, v.r * 255.0f))),
+          g(static_cast<unsigned char>(std::fmax(0, v.g * 255.0f))),
+          b(static_cast<unsigned char>(std::fmax(0, v.b * 255.0f))),
+          a(static_cast<unsigned char>(std::fmax(0, v.a * 255.0f))) {}
 };
 
 class Renderer {
@@ -45,14 +47,37 @@ class Renderer {
     void render();
 };
 
-glm::vec4 Renderer::frag(glm::vec2 uv) { return glm::vec4(uv.r, uv.g, 0, 1); }
+glm::vec4 Renderer::frag(glm::vec2 coord) {
+    // a =(bx^2 + by^2 + bz ^ 2)
+    // b = 2 * (ax*bx + ay*by + az * bz)
+    // c = (ax^2 + ay^2 + az^2 - r^2)
+    // a*t^2 + bt + c = 0
+    //
+
+    glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+    glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+    rayDirection = glm::normalize(rayDirection);
+    float radius = 0.5f;
+
+    float a = glm::dot(rayDirection, rayDirection);
+    float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+    float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+    float discriminant = b * b - 4.0f * a * c;
+    if (discriminant >= 0.0f)
+        return glm::vec4(1, 0, 1, 1);
+
+    return glm::vec4(0, 0, 0, 1);
+}
 
 void Renderer::render() {
-    for (int y = 0; y < width; y++) {
-        for (int x = 0; x < height; x++) {
-            float u = y / (float)width;
-            float v = x / (float)height;
-            pixels[x * width + y] = (Pixel)frag(glm::vec2(u, v));
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            float u = x / (float)width;
+            float v = y / (float)height;
+            auto coord = glm::vec2(u, v) * 2.0f - 1.0f;
+            coord.x *= width / (float)height;
+            pixels[x + y * width] = (Pixel)frag(coord);
         }
     }
     tex.setPixels((unsigned char *)pixels);
