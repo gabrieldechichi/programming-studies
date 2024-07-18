4f5850f14c1f9e327ca1ab0e2cd6a93862c54513
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-07-17 20:17:43 -0700

    raytracing-cherno: control light dir

--- gamedev/graphics/raytracing-cherno/src/main.cpp
+++ gamedev/graphics/raytracing-cherno/src/main.cpp
@@ -1,4 +1,5 @@
 #include "glm/common.hpp"
+#include "glm/fwd.hpp"
 #include "glm/geometric.hpp"
 #include "glm/glm.hpp"
 #include "imgui.h"
@@ -27,12 +28,27 @@ struct Pixel {
           a(static_cast<unsigned char>(std::fmax(0, v.a * 255.0f))) {}
 };
 
+struct Light {
+    glm::vec3 dir;
+    glm::vec3 color;
+
+    Light() = default;
+    Light(glm::vec3 dir, glm::vec3 color)
+        : dir(glm::normalize(dir)), color(color) {}
+};
+
+struct FrameData {
+    glm::vec2 coord;
+    Light light;
+};
+
 class Renderer {
   public:
     Renderer(uint32_t width, uint32_t height)
         : width(width), height(height), pixelLen(width * height) {
         pixels = new Pixel[pixelLen];
         tex = Texture::create(width, height);
+        light = Light(glm::vec3(-1, -1, 1), glm::vec3(1));
     }
 
     ~Renderer() {
@@ -44,17 +60,19 @@ class Renderer {
     Pixel *pixels;
     int pixelLen;
     Texture tex;
-    glm::vec4 frag(glm::vec2 uv);
+    Light light;
+
+    glm::vec4 frag(const FrameData &frame);
     void render();
 };
 
-glm::vec4 Renderer::frag(glm::vec2 coord) {
-    glm::vec3 lightDir(-1,-1,1);
-    lightDir = glm::normalize(lightDir);
+glm::vec4 Renderer::frag(const FrameData &frame) {
+    auto coord = frame.coord;
+    auto light = frame.light;
 
     glm::vec3 rayOrigin(0.0f, 0.0f, -2.0f);
     glm::vec3 rayDirection(coord.x, coord.y, 1.0f);
-    //rayDirection = glm::normalize(rayDirection);
+    // rayDirection = glm::normalize(rayDirection);
     float radius = 0.5f;
 
     float a = glm::dot(rayDirection, rayDirection);
@@ -67,29 +85,30 @@ glm::vec4 Renderer::frag(glm::vec2 coord) {
     }
 
     float discriminantSqroot = std::sqrt(discriminant);
-    float t1 = (-b + discriminantSqroot) / (2 * a);
-    float t2 = (-b - discriminantSqroot) / (2 * a);
-
-    float t = t1 < t2 ? t1 : t2;
+    // a is always positive so -b - discriminantSqroot is the closest t
+    float t = (-b - discriminantSqroot) / (2 * a);
 
     auto hitPoint = rayOrigin + rayDirection * t;
     auto normal = glm::normalize(hitPoint);
     // normal = normal * 0.5f + 0.5f;
-    auto diffuse = glm::dot(normal, -lightDir);
-    auto color = glm::vec3(1,1,1);
+    auto diffuse = glm::max(0.0f, glm::dot(normal, -light.dir));
+    auto color = glm::vec3(1, 0, 1);
     color *= diffuse;
 
     return glm::vec4(color, 1);
 }
 
 void Renderer::render() {
+    FrameData frameData;
+    frameData.light = light;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             float u = x / (float)width;
             float v = y / (float)height;
             auto coord = glm::vec2(u, v) * 2.0f - 1.0f;
             coord.x *= width / (float)height;
-            pixels[x + y * width] = (Pixel)frag(coord);
+            frameData.coord = coord;
+            pixels[x + y * width] = (Pixel)frag(frameData);
         }
     }
     tex.setPixels((unsigned char *)pixels);
@@ -110,6 +129,8 @@ int main() {
 
     auto renderer = Renderer(width, height);
 
+    float lastTime = glfwGetTime();
+
     while (!platform->shouldClose()) {
         platform->beginFrame();
 
@@ -117,13 +138,36 @@ int main() {
             glfwSetWindowShouldClose(platform->window, true);
         }
 
-        // ImGui::Begin("Demo Window");
-        // ImGui::Text("hello world");
-        // ImGui::End();
-        //
-        // ImGui::Begin("Texture window");
-        // ImGui::Image((void *)(intptr_t)tex.id, ImVec2(width, height));
-        // ImGui::End();
+        float currentTime = glfwGetTime();
+        float dt = currentTime - lastTime;
+        lastTime = currentTime;
+
+        {
+            ImGui::SetNextWindowPos(ImVec2((float)width, (float)0),
+                                    ImGuiCond_Always, ImVec2(1.0, 0.0));
+            ImGui::SetNextWindowSize(ImVec2(250, height));
+
+            ImGui::Begin("Property editor");
+            ImGui::Text("Light direction");
+            if (ImGui::DragFloat3("", &renderer.light.dir[0], 0.2f)) {
+                renderer.light.dir = glm::normalize(renderer.light.dir);
+            }
+            ImGui::End();
+        }
+        // fps
+        {
+            ImGui::SetNextWindowPos(ImVec2((float)0, (float)0),
+                                    ImGuiCond_Always);
+            ImGui::Begin("Frame Time Info", NULL,
+                         ImGuiWindowFlags_NoDecoration |
+                             ImGuiWindowFlags_NoMove |
+                             ImGuiWindowFlags_AlwaysAutoResize |
+                             ImGuiWindowFlags_NoSavedSettings |
+                             ImGuiWindowFlags_NoFocusOnAppearing |
+                             ImGuiWindowFlags_NoNav);
+            ImGui::Text("Frame Time: %.3f ms", dt * 1000);
+            ImGui::End();
+        }
 
         platform->beginRender();
         renderer.render();
