96d0e80184f288292f722f2bf0124e00d7662d9b
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-07-27 17:30:15 -0700

    shadertoy: (wip) rotating raymarching cylinder with diffuse lighting

--- gamedev/graphics/shadertoy/initial.glsl
+++ gamedev/graphics/shadertoy/initial.glsl
@@ -19,6 +19,39 @@ float opSmoothIntersection(float d1, float d2, float k)
     return mix(d2, d1, h) + k * h * (1.0 - h);
 }
 
+float sdCylinder(vec3 p, vec3 a, vec3 b, float r)
+{
+    vec3 ba = b - a;
+    vec3 pa = p - a;
+    float baba = dot(ba, ba);
+    float paba = dot(pa, ba);
+    float x = length(pa * baba - ba * paba) - r * baba;
+    float y = abs(paba - baba * 0.5) - baba * 0.5;
+    float x2 = x * x;
+    float y2 = y * y * baba;
+
+    float d = (max(x, y) < 0.0) ? -min(x2, y2) : (((x > 0.0) ? x2 : 0.0) + ((y > 0.0) ? y2 : 0.0));
+
+    return sign(d) * sqrt(abs(d)) / baba;
+}
+
+float sdCylinderY(vec3 p, float h, float r)
+{
+    vec2 d = abs(vec2(length(p.xz), p.y)) - vec2(r, h);
+    return min(max(d.x, d.y), 0.0) + length(max(d, 0.0));
+}
+float sdCylinderX(vec3 p, float h, float r)
+{
+    vec2 d = abs(vec2(length(p.yz), p.x)) - vec2(r, h);
+    return min(max(d.x, d.y), 0.0) + length(max(d, 0.0));
+}
+
+float sdCylinderZ(vec3 p, float h, float r)
+{
+    vec2 d = abs(vec2(length(p.xy), p.z)) - vec2(r, h);
+    return min(max(d.x, d.y), 0.0) + length(max(d, 0.0));
+}
+
 float vignette(vec2 uv, float r) {
     return min(1.0 - length(uv) + r, 1.0);
 }
@@ -46,12 +79,27 @@ float sdBox(vec3 p, vec3 bounds) {
     return length(max(abs(p) - bounds, 0.0));
 }
 
-float rayMarchScene(vec3 p) {
-    vec4 sphere = vec4(1.5 * sin(iTime), 0., 0.0, 1.);
-    float sphereDist = sdSphere(p - sphere.xyz, sphere.w);
+vec3 rotateYY(vec3 point, float angle)
+{
+    float r = angle;
+    float x = point.x * sin(r) - point.z * cos(r);
+    float z = point.x * cos(r) + point.z * sin(r);
+    return vec3(x, point.y, z);
+}
 
-    float boxDist = sdBox(p, vec3(0.75));
-    return opSmoothUnion(sphereDist, boxDist, 0.5);
+float map(vec3 p) {
+    p = rotateYY(p, iTime);
+    return sdCylinderZ(p, 0.15, 1.0);
+}
+
+vec3 calcNormal(in vec3 pos)
+{
+    vec2 e = vec2(1.0, -1.0) * 0.5773;
+    const float eps = EPSILON;
+    return normalize(e.xyy * map(pos + e.xyy * eps) +
+            e.yyx * map(pos + e.yyx * eps) +
+            e.yxy * map(pos + e.yxy * eps) +
+            e.xxx * map(pos + e.xxx * eps));
 }
 
 const float MAX_RM_DISTANCE = 200.0;
@@ -65,19 +113,28 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     vec3 ro = vec3(0., 0., -3.);
     vec3 rd = normalize(vec3(uv, 1.));
     vec3 col = vec3(0.);
+    vec3 lightDir = vec3(0.5773);
+    vec3 lightCol = vec3(1.0);
 
     float t = 0.;
     for (int i = 0; i < MAX_RM_IT; i++) {
         vec3 p = ro + rd * t;
-        float d = rayMarchScene(p);
+        float d = map(p);
         t += d;
         if (d < MIN_RM_DISTANCE) break;
         if (t > MAX_RM_DISTANCE) break;
     }
 
-    col = vec3(t * 0.2);
-    vec3 bg = background(uv);
-    col = mix(col, bg, step(MAX_RM_DISTANCE, t));
+    if (t < MAX_RM_DISTANCE) {
+        vec3 pos = ro + rd * t;
+        vec3 normal = calcNormal(pos);
+        float diffuse = max(dot(normal, lightDir), 0.0);
+        vec3 amb = vec3(0.2);
+        col = vec3(diffuse) + amb;
+        col = normal;
+    }
+    // vec3 bg = background(uv);
+    // col = mix(col, bg, step(MAX_RM_DISTANCE, t));
 
     fragColor = vec4(col, 1.0);
 }
