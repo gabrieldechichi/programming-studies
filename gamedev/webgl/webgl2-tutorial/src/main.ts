cb7b1d8796ab900a1ef409fc7768fe75d14e7dfb
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-09-25 20:55:49 -0700

    webgl2-tutorial: multi texture bind + abstractins

--- gamedev/webgl/webgl2-tutorial/src/main.ts
+++ gamedev/webgl/webgl2-tutorial/src/main.ts
@@ -2,6 +2,90 @@ class Constants {
   static readonly Float32Size = Float32Array.BYTES_PER_ELEMENT;
 }
 
+async function loadImage(
+  src: string,
+  allowCors?: boolean,
+): Promise<HTMLImageElement> {
+  return new Promise((resolve) => {
+    const image = new Image();
+    if (allowCors) {
+      image.crossOrigin = "anonymous";
+    }
+    image.addEventListener("load", () => resolve(image));
+    image.src = src;
+  });
+}
+
+enum TexFormat {
+  RGB = WebGL2RenderingContext.RGB,
+  RGBA = WebGL2RenderingContext.RGBA,
+}
+
+enum TexFiltering {
+  Nearest = WebGL2RenderingContext.NEAREST,
+  Linear = WebGL2RenderingContext.LINEAR,
+}
+
+type CreateTexParamsBase = {
+  gl: WebGL2RenderingContext;
+  texIndex: number;
+
+  skipFlipY?: boolean;
+  format: TexFormat;
+  minFilter: TexFiltering;
+  magFilter: TexFiltering;
+};
+
+type CreateTexFromImgParams = CreateTexParamsBase & {
+  image: HTMLImageElement;
+};
+type CreateTexFromPixelsParams = CreateTexParamsBase & {
+  //todo: other array types
+  pixels: Uint8Array;
+  width: number;
+  height: number;
+};
+
+class graphics {
+  static async createAndBindTexture(
+    params: CreateTexFromImgParams | CreateTexFromPixelsParams,
+  ) {
+    const { gl, texIndex, skipFlipY, format, minFilter, magFilter } = params;
+    gl.activeTexture(gl.TEXTURE0 + texIndex);
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, skipFlipY || true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilter);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilter);
+
+    if ("image" in params) {
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        format,
+        params.image.width,
+        params.image.height,
+        0,
+        format,
+        gl.UNSIGNED_BYTE,
+        params.image,
+      );
+    } else if ("pixels" in params) {
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        format,
+        params.width,
+        params.height,
+        0,
+        format,
+        gl.UNSIGNED_BYTE,
+        params.pixels,
+      );
+    }
+  }
+}
+
 async function main() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const gl = canvas.getContext("webgl2");
@@ -41,11 +125,15 @@ in vec3 vColor;
 in vec2 vTexCoord;
 
 uniform sampler2D uSampler;
+uniform sampler2D uSamplerCat;
 
 void main() {
     // vec3 color = vColor;
     vec3 color = vec3(1.);
-    color *= texture(uSampler, vTexCoord).rgb;
+    vec3 tex1 = texture(uSampler, vTexCoord).rgb;
+    vec3 tex2 = texture(uSamplerCat, vTexCoord).rgb;
+    color *= tex1;
+    color *= tex2;
     fragColor = vec4(color, 1.0);
 }
   `;
@@ -115,6 +203,12 @@ void main() {
     (2 + 2) * Constants.Float32Size,
   );
 
+  const uSampler = gl.getUniformLocation(program, "uSampler");
+  gl.uniform1i(uSampler, 0);
+
+  const uSamplerCat = gl.getUniformLocation(program, "uSamplerCat");
+  gl.uniform1i(uSamplerCat, 1);
+
   const pixels = new Uint8Array([
     255, 255, 255, 230, 25, 75, 60, 180, 75, 255, 225, 25, 67, 99, 216, 245,
     130, 49, 145, 30, 180, 70, 240, 240, 240, 50, 230, 188, 246, 12, 250, 190,
@@ -122,26 +216,30 @@ void main() {
   ]);
 
   // bind texture 0
-  gl.activeTexture(gl.TEXTURE0);
-  const uSampler = gl.getUniformLocation(program, "uSampler");
-  gl.uniform1i(uSampler, 0);
-
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,
-    gl.RGB,
-    4,
-    4,
-    0,
-    gl.RGB,
-    gl.UNSIGNED_BYTE,
+  graphics.createAndBindTexture({
+    gl,
+    texIndex: 0,
+    format: TexFormat.RGB,
+    minFilter: TexFiltering.Nearest,
+    magFilter: TexFiltering.Nearest,
     pixels,
+    width: 4,
+    height: 4,
+  });
+
+  const cat = await loadImage(
+    "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg",
+    true,
   );
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  graphics.createAndBindTexture({
+    gl,
+    texIndex: 1,
+    format: TexFormat.RGBA,
+    minFilter: TexFiltering.Linear,
+    magFilter: TexFiltering.Linear,
+    image: cat,
+  });
 
   const uScale = gl.getUniformLocation(program, "uScale");
   gl.uniform1f(uScale, 1.5);
