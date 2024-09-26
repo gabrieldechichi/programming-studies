3175d57c87197999ad0851ae46a0532940697a3c
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-09-26 16:36:44 -0700

    webgl2-tutorial: vertex array

--- gamedev/webgl/webgl2-tutorial/src/main.ts
+++ gamedev/webgl/webgl2-tutorial/src/main.ts
@@ -1,4 +1,3 @@
-import * as graphics from "./graphics";
 import * as constants from "./constants";
 
 async function main() {
@@ -12,20 +11,19 @@ async function main() {
 
   const vertexSource = `#version 300 es
 
-  uniform float uScale;
   in vec2 aPosition;
-  in vec2 aTexCoord;
+  in float aPointSize;
   in vec3 aColor;
 
   out vec3 vColor;
-  out vec2 vTexCoord;
 
   void main() {
       vColor = aColor;
-      vTexCoord = aTexCoord;
-      gl_Position = vec4(aPosition * uScale, 0.0, 1.0);
+      gl_PointSize = aPointSize;
+      gl_Position = vec4(aPosition, 0.0, 1.0);
   }
   `;
+
   const vertexShader = gl.createShader(gl.VERTEX_SHADER)!;
   gl.shaderSource(vertexShader, vertexSource);
   gl.compileShader(vertexShader);
@@ -34,24 +32,14 @@ async function main() {
   const fragSource = `#version 300 es
 precision mediump float;
 
+in vec3 vColor;
 out vec4 fragColor;
 
-in vec3 vColor;
-in vec2 vTexCoord;
-
-uniform sampler2D uSampler;
-uniform sampler2D uSamplerCat;
-
 void main() {
-    // vec3 color = vColor;
-    vec3 color = vec3(1.);
-    vec3 tex1 = texture(uSampler, vTexCoord).rgb;
-    vec3 tex2 = texture(uSamplerCat, vTexCoord).rgb;
-    color *= tex1;
-    color *= tex2;
-    fragColor = vec4(color, 1.0);
+    fragColor = vec4(vColor, 1.0);
 }
   `;
+
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
   gl.shaderSource(fragmentShader, fragSource);
   gl.compileShader(fragmentShader);
@@ -66,100 +54,131 @@ void main() {
 
   gl.useProgram(program);
 
-  // prettier-ignore
-  const vertexBufferData = new Float32Array([
-      //pos          //uv      //color
-      -0.5, -0.5,    0,0,      1, 0, 0, 
-      +0.5, -0.5,    1,0,      0, 1, 0,
-      +0.5, +0.5,    1,1,      0, 0, 1,  
-      +0.5, +0.5,    1,1,      0, 0, 1,  
-      -0.5, +0.5,    0,1,      0, 0, 1,  
-      -0.5, -0.5,    0,0,      1, 0, 0, 
-  ])
+  const aPosition = gl.getAttribLocation(program, "aPosition");
+  const aPointSize = gl.getAttribLocation(program, "aPointSize");
+  const aColor = gl.getAttribLocation(program, "aColor");
 
   //number of floats per vertex
-  const strideFloatCount = 7;
+  const strideFloatCount = 6;
   const strideBytes = strideFloatCount * constants.FLOAT_SIZE;
 
-  const vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertexBufferData, gl.STATIC_DRAW);
+  let vao1: WebGLVertexArrayObject | null;
+  //bind vao1
+  {
+    //prettier-ignore
+    const data1 = new Float32Array([
+      //pos         //color         //size
+      -0.8, 0.6,    1, 0.75, 0.75,  125,
+      -0.3, 0.6,    0, 0.75, 1,     32,
+      0.3, 0.6,     0.5, 1, 0.75,   75,
+      0.8, 0.6,     0, 0.75, 0.75,  9,
+    ]);
 
-  const aPosition = gl.getAttribLocation(program, "aPosition");
-  const aTexCoord = gl.getAttribLocation(program, "aTexCoord");
-  const aColor = gl.getAttribLocation(program, "aColor");
-  gl.enableVertexAttribArray(aPosition);
-  gl.enableVertexAttribArray(aTexCoord);
-  gl.enableVertexAttribArray(aColor);
+    const bufferData1 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferData1);
+    gl.bufferData(gl.ARRAY_BUFFER, data1, gl.STATIC_DRAW);
 
-  gl.vertexAttribPointer(
-    aPosition,
-    2,
-    gl.FLOAT,
-    false,
-    strideBytes,
-    0 * constants.FLOAT_SIZE,
-  );
-  gl.vertexAttribPointer(
-    aTexCoord,
-    2,
-    gl.FLOAT,
-    false,
-    strideBytes,
-    2 * constants.FLOAT_SIZE,
-  );
+    vao1 = gl.createVertexArray();
+    gl.bindVertexArray(vao1);
 
-  gl.vertexAttribPointer(
-    aColor,
-    3,
-    gl.FLOAT,
-    false,
-    strideBytes,
-    (2 + 2) * constants.FLOAT_SIZE,
-  );
+    gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aPointSize);
+    gl.enableVertexAttribArray(aColor);
 
-  const uSampler = gl.getUniformLocation(program, "uSampler");
-  gl.uniform1i(uSampler, 0);
+    gl.vertexAttribPointer(
+      aPosition,
+      2,
+      gl.FLOAT,
+      false,
+      strideBytes,
+      0 * constants.FLOAT_SIZE,
+    );
+    gl.vertexAttribPointer(
+      aColor,
+      3,
+      gl.FLOAT,
+      false,
+      strideBytes,
+      2 * constants.FLOAT_SIZE,
+    );
 
-  const uSamplerCat = gl.getUniformLocation(program, "uSamplerCat");
-  gl.uniform1i(uSamplerCat, 1);
+    gl.vertexAttribPointer(
+      aPointSize,
+      1,
+      gl.FLOAT,
+      false,
+      strideBytes,
+      (2 + 3) * constants.FLOAT_SIZE,
+    );
 
-  const pixels = new Uint8Array([
-    255, 255, 255, 230, 25, 75, 60, 180, 75, 255, 225, 25, 67, 99, 216, 245,
-    130, 49, 145, 30, 180, 70, 240, 240, 240, 50, 230, 188, 246, 12, 250, 190,
-    190, 0, 128, 128, 230, 190, 255, 154, 99, 36, 255, 250, 200, 0, 0, 0,
-  ]);
+    gl.bindVertexArray(null);
+  }
 
-  // bind texture 0
-  graphics.createAndBindTexture({
-    gl,
-    texIndex: 0,
-    format: graphics.TexFormat.RGB,
-    minFilter: graphics.TexFiltering.Nearest,
-    magFilter: graphics.TexFiltering.Nearest,
-    pixels,
-    width: 4,
-    height: 4,
-  });
+  let vao2: WebGLVertexArrayObject | null;
+  {
+    //prettier-ignore
+    const data2 = new Float32Array([
+      //position      //color        //size
+      -0.8, -0.6,     0.25, 0, 0,    25,
+      -0.3, -0.6,     0, 0, 0.25,    132,
+      0.3, -0.6,      0, 0.25, 0,    105,
+      0.6, -0.6,      0.25, 0, 0.25, 90,
+    ]);
 
-  const cat = await graphics.loadImage(
-    "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg",
-    true,
-  );
+    const bufferData2 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferData2);
+    gl.bufferData(gl.ARRAY_BUFFER, data2, gl.STATIC_DRAW);
 
-  graphics.createAndBindTexture({
-    gl,
-    texIndex: 1,
-    format: graphics.TexFormat.RGBA,
-    minFilter: graphics.TexFiltering.Linear,
-    magFilter: graphics.TexFiltering.Linear,
-    image: cat,
-  });
+    vao2 = gl.createVertexArray();
+    gl.bindVertexArray(vao2);
 
-  const uScale = gl.getUniformLocation(program, "uScale");
-  gl.uniform1f(uScale, 1.5);
+    gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aColor);
+    gl.enableVertexAttribArray(aPointSize);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
+    gl.vertexAttribPointer(
+      aPosition,
+      2,
+      gl.FLOAT,
+      false,
+      strideBytes,
+      0 * constants.FLOAT_SIZE,
+    );
+    gl.vertexAttribPointer(
+      aColor,
+      3,
+      gl.FLOAT,
+      false,
+      strideBytes,
+      2 * constants.FLOAT_SIZE,
+    );
+    gl.vertexAttribPointer(
+      aPointSize,
+      1,
+      gl.FLOAT,
+      false,
+      strideBytes,
+      (2 + 3) * constants.FLOAT_SIZE,
+    );
+
+    gl.bindVertexArray(null);
+  }
+
+  function draw() {
+    if (!gl) {
+      return;
+    }
+    gl.bindVertexArray(vao1);
+    gl.drawArrays(gl.POINTS, 0, 4);
+
+    gl.bindVertexArray(vao2);
+    gl.drawArrays(gl.POINTS, 0, 4);
+    gl.bindVertexArray(null);
+
+    requestAnimationFrame(draw);
+  }
+
+  draw();
 }
 
 main()
--- gamedev/webgl/webgl2-tutorial/src/ref.ts
+++ gamedev/webgl/webgl2-tutorial/src/ref.ts
@@ -1,112 +1,110 @@
-const vertexShaderSrc = `#version 300 es
+// tslint:disable: no-console
+
+const vss1 = `#version 300 es
 #pragma vscode_glsllint_stage: vert
 
-layout(location=0) in vec4 aPosition;
-layout(location=1) in vec2 aTexCoord;
+layout(location=0) in float aPointSize;
+layout(location=1) in vec2 aPosition;
+layout(location=2) in vec3 aColor;
 
-out vec2 vTexCoord;
+out vec3 vColor;
+
 void main()
 {
-	vTexCoord = aTexCoord;
-    gl_Position = aPosition;
+    vColor = aColor;
+    gl_PointSize = aPointSize;
+    gl_Position = vec4(aPosition, 0.0, 1.0);
 }`;
 
-const fragmentShaderSrc = `#version 300 es
+const fss1 = `#version 300 es
 #pragma vscode_glsllint_stage: frag
 
 precision mediump float;
 
-in vec2 vTexCoord;
-
-uniform sampler2D uSampler;
+in vec3 vColor;
 
 out vec4 fragColor;
 
 void main()
 {
-    fragColor = texture(uSampler, vTexCoord);
+    fragColor = vec4(vColor, 1.0);
 }`;
 
 const gl = document.querySelector("canvas").getContext("webgl2");
 
 const program = gl.createProgram();
-{
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderSrc);
-  gl.compileShader(vertexShader);
-  gl.attachShader(program, vertexShader);
 
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fragmentShaderSrc);
-  gl.compileShader(fragmentShader);
-  gl.attachShader(program, fragmentShader);
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vss1);
+gl.compileShader(vertexShader);
+gl.attachShader(program, vertexShader);
 
-  gl.linkProgram(program);
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fss1);
+gl.compileShader(fragmentShader);
+gl.attachShader(program, fragmentShader);
 
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.log(gl.getShaderInfoLog(vertexShader));
-    console.log(gl.getShaderInfoLog(fragmentShader));
-  }
+gl.linkProgram(program);
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  console.log(gl.getShaderInfoLog(vertexShader));
+  console.log(gl.getShaderInfoLog(fragmentShader));
+  console.log(gl.getProgramInfoLog(program));
+  throw new Error("failed to link");
 }
+
 gl.useProgram(program);
 
-const vertexBufferData = new Float32Array([
-  -0.9, 0.9, -0.9, -0.9, 0.9, 0.9, 0.9, -0.9,
+const data1 = new Float32Array([
+  -0.8, 0.6, 1, 0.75, 0.75, 125, -0.3, 0.6, 0, 0.75, 1, 32, 0.3, 0.6, 0.5, 1,
+  0.75, 75, 0.8, 0.6, 0, 0.75, 0.75, 9,
 ]);
+const buffer1 = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
+gl.bufferData(gl.ARRAY_BUFFER, data1, gl.STATIC_DRAW);
 
-const texCoordBufferData = new Float32Array([0, 1, 0, 0, 1, 1, 1, 0]);
+const vao1 = gl.createVertexArray();
+gl.bindVertexArray(vao1);
 
-const pixels = new Uint8Array([
-  255, 255, 255, 230, 25, 75, 60, 180, 75, 255, 225, 25, 67, 99, 216, 245, 130,
-  49, 145, 30, 180, 70, 240, 240, 240, 50, 230, 188, 246, 12, 250, 190, 190, 0,
-  128, 128, 230, 190, 255, 154, 99, 36, 255, 250, 200, 0, 0, 0,
-]);
+gl.vertexAttribPointer(0, 1, gl.FLOAT, false, 24, 20);
+gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 24, 0);
+gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 24, 8);
 
-const pixelBuffer = gl.createBuffer();
-gl.bindBuffer(gl.PIXEL_UNPACK_BUFFER, pixelBuffer);
-gl.bufferData(gl.PIXEL_UNPACK_BUFFER, pixels, gl.STATIC_DRAW);
-
-const vertexBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertexBufferData, gl.STATIC_DRAW);
-gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(0);
-
-const texCoordBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, texCoordBufferData, gl.STATIC_DRAW);
-gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(1);
+gl.enableVertexAttribArray(2);
 
-const loadImage = () =>
-  new Promise((resolve) => {
-    const image = new Image();
-    image.addEventListener("load", () => resolve(image));
-    image.src = "./image.png";
-  });
+gl.bindVertexArray(null);
 
-const run = async () => {
-  const image = await loadImage();
+const data2 = new Float32Array([
+  -0.8, -0.6, 0.25, 0, 0, 25, -0.3, -0.6, 0, 0, 0.25, 132, 0.3, -0.6, 0, 0.25,
+  0, 105, 0.6, -0.6, 0.25, 0, 0.25, 90,
+]);
+const buffer2 = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer2);
+gl.bufferData(gl.ARRAY_BUFFER, data2, gl.STATIC_DRAW);
 
-  // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+const vao2 = gl.createVertexArray();
+gl.bindVertexArray(vao2);
 
-  const textureSlot = 1;
-  gl.activeTexture(gl.TEXTURE0 + textureSlot);
-  gl.uniform1i(gl.getUniformLocation(program, "uSampler"), textureSlot);
+gl.vertexAttribPointer(0, 1, gl.FLOAT, false, 24, 20);
+gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 24, 0);
+gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 24, 8);
 
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 500,300, 0, gl.RGB, gl.UNSIGNED_BYTE, image);
-  // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 4,4, 0, gl.RGB, gl.UNSIGNED_BYTE, pixels);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 4, 4, 0, gl.RGB, gl.UNSIGNED_BYTE, 0);
+gl.enableVertexAttribArray(0);
+gl.enableVertexAttribArray(1);
+gl.enableVertexAttribArray(2);
 
-  gl.generateMipmap(gl.TEXTURE_2D);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+gl.bindVertexArray(null);
 
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+const draw = () => {
+  gl.bindVertexArray(vao1);
+  gl.drawArrays(gl.POINTS, 0, 4);
+  gl.bindVertexArray(vao2);
+  gl.drawArrays(gl.POINTS, 0, 4);
+  gl.bindVertexArray(null);
+
+  requestAnimationFrame(draw);
 };
 
-run();
+draw();
