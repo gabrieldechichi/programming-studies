079ce2ffa8078f3d4e98e5931791dd328cb665ad
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-10-05 14:40:37 -0700

    babylonjs play with lights

--- gamedev/babylonjs/babylon-sandbox/src/main.ts
+++ gamedev/babylonjs/babylon-sandbox/src/main.ts
@@ -28,14 +28,27 @@ async function main() {
   engine = new b.Engine(canvas);
   scene = new b.Scene(engine);
   // scene.createDefaultLight();
-  scene.ambientColor = new b.Color3(0, 0.6, 0.6)
+  // scene.ambientColor = new b.Color3(0, 0.6, 0.6);
+
+  const light = new b.PointLight("point light", new b.Vector3(0, 2, 0));
+  light.range = 100;
+  light.intensity = 0.5;
 
   const camera = new b.UniversalCamera("camera", new b.Vector3(0, 1, -5));
   camera.setTarget(new b.Vector3(0, 0, 0));
-  camera.attachControl(true)
-  camera.inputs.addMouseWheel()
+  camera.attachControl(true);
+  camera.inputs.addMouseWheel();
   const box = b.MeshBuilder.CreateBox("box");
   box.position.y += box.getBoundingInfo().boundingBox.extendSizeWorld.y;
+
+  const boxMat = new b.StandardMaterial("box mat");
+  boxMat.diffuseColor = b.Color3.White();
+  boxMat.ambientColor = b.Color3.White();
+  box.material = boxMat;
+
+  const utilLayer = new b.UtilityLayerRenderer(scene);
+  const positionGizmos = new b.PositionGizmo(utilLayer);
+  positionGizmos.attachedMesh = box;
 
   const ground = b.MeshBuilder.CreateGround("ground", {
     width: 10,
@@ -44,10 +57,13 @@ async function main() {
   });
 
   const groundMat = new b.StandardMaterial("ground mat");
-  groundMat.diffuseColor = b.Color3.White()
-  ground.material =groundMat
-  groundMat.ambientColor = b.Color3.White()
-  groundMat.emissiveColor = new b.Color3(0.2)
+  groundMat.diffuseColor = b.Color3.White();
+  groundMat.ambientColor = b.Color3.White();
+  groundMat.emissiveColor = new b.Color3(0.2);
+  ground.material = groundMat;
+
+  const lightGizmos = new b.LightGizmo(utilLayer);
+  lightGizmos.light = light;
 
   engine.runRenderLoop(update);
 }
