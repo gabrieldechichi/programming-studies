6caf83345c3d99eee36a14556059fa06d7acd196
Gabriel Dechichi <gabrieldechichi@gmail.com>
2024-10-07 14:30:05 -0700

    babylonjs: import models from unity pack

--- gamedev/babylonjs/babylon-sandbox/src/engine/prelude.ts
+++ gamedev/babylonjs/babylon-sandbox/src/engine/prelude.ts
@@ -1,8 +1,15 @@
-export * from "./animations"
+export * from "./animations";
 
-export function enforceAspectRatio(canvas: HTMLCanvasElement, aspectRatio: number) {
+export function enforceAspectRatio(
+  canvas: HTMLCanvasElement,
+  aspectRatio: number,
+) {
   const width = window.innerWidth;
   const height = width / aspectRatio;
   canvas.width = width;
   canvas.height = height;
+}
+
+export function waitForMs(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- gamedev/babylonjs/babylon-sandbox/src/main.ts
+++ gamedev/babylonjs/babylon-sandbox/src/main.ts
@@ -1,6 +1,7 @@
 import * as b from "@babylonjs/core";
 import "@babylonjs/loaders";
 import * as e from "./engine/prelude";
+import { Inspector } from "@babylonjs/inspector";
 
 const ASPECT_RATIO = 1920 / 1080;
 
@@ -10,6 +11,11 @@ let scene: b.Scene;
 
 function update() {
   scene.render();
+
+  // const camera = scene.cameras[0] as b.ArcRotateCamera
+  // camera.alpha = -1.3;
+  // camera.beta = 1.3;
+  // camera.radius = 10.3;
 }
 
 function buildHierarchyDict(rootNodes: b.Node[]): e.TransformHierarchyDict {
@@ -40,8 +46,9 @@ async function main() {
   engine = new b.Engine(canvas);
   scene = new b.Scene(engine);
 
-  const light = new b.DirectionalLight("dir light", new b.Vector3(-2, -3, 0));
-  light.intensity = 1;
+  scene.createDefaultLight();
+  // const light = new b.DirectionalLight("dir light", new b.Vector3(-2, -3, 0));
+  // light.intensity = 1;
 
   const camera = new b.ArcRotateCamera(
     "camera",
@@ -70,53 +77,43 @@ async function main() {
   // groundMat.emissiveColor = new b.Color3(0.2);
   ground.material = groundMat;
 
-  const lightGizmos = new b.LightGizmo(utilLayer);
-  lightGizmos.light = light;
+  // const lightGizmos = new b.LightGizmo(utilLayer);
+  // lightGizmos.light = light;
 
   const xbotAc = await b.SceneLoader.LoadAssetContainerAsync(
     "/models/",
-    "XBot.glb",
+    "Body_4.glb",
   );
+  xbotAc.materials[0].dispose()
+  const pbrMat = new b.StandardMaterial("Main");
+  pbrMat.specularColor = b.Color3.Black()
+  const mainTex = new b.Texture("/textures/Colors_Tex.png");
+  mainTex.vScale = -1;
+  pbrMat.diffuseTexture = mainTex;
+  // pbrMat.albedoTexture = mainTex;
+  // pbrMat.albedoColor = b.Color3.White();
+  xbotAc.materials = [pbrMat];
 
-  const idleAnim = await e.loadTargetedAnimationData(
-    "/animations/",
-    "Idle.glb",
-  );
-  const runAnim = await e.loadTargetedAnimationData(
-    "/animations/",
-    "Fast Run.glb",
-  );
+  // (xbotAc.materials[0] as b.PBRMaterial).albedoColor = b.Color3.Green();
+  // const idleAnim = await e.loadTargetedAnimationData(
+  //   "/animations/",
+  //   "Idle.glb",
+  // );
+  // const runAnim = await e.loadTargetedAnimationData(
+  //   "/animations/",
+  //   "Fast Run.glb",
+  // );
 
-  const xbot = new b.TransformNode("xbot");
-  const xbotMesh = xbotAc.instantiateModelsToScene((name) => name);
-  const transformDict = buildHierarchyDict(xbotMesh.rootNodes);
+  const xbot = xbotAc.instantiateModelsToScene((name) => name);
+  xbot.rootNodes[0].getChildMeshes()[0].material = pbrMat;
+  console.log();
+  const mesh = xbot.rootNodes[0] as b.Mesh;
 
-  xbotMesh.rootNodes[0].parent = xbot;
-  xbotMesh.rootNodes[0].name = "graphics";
+  console.log(mesh);
 
-  e.addTargetedAnimationGroup(
-    xbotMesh.animationGroups,
-    idleAnim,
-    transformDict,
-  );
-  e.addTargetedAnimationGroup(
-    xbotMesh.animationGroups,
-    runAnim,
-    transformDict,
-  );
-
-  xbotMesh.animationGroups[0].stop();
-  xbotMesh.animationGroups[0].weight = 0.5
-  xbotMesh.animationGroups[0].play();
-  xbotMesh.animationGroups[0].loopAnimation = true;
-
-  xbotMesh.animationGroups[1].stop();
-  xbotMesh.animationGroups[1].weight = 0.5
-  xbotMesh.animationGroups[1].play();
-  xbotMesh.animationGroups[1].loopAnimation = true;
-
-  camera.alpha = 1.3;
+  camera.alpha = -1.3;
   camera.beta = 1.3;
+  camera.radius = 10.3;
 
   engine.runRenderLoop(update);
   // Inspector.Show(scene, {});
