7f5e296d0ddfd6b5cdad7cbaed426b8b4a1413c9
Gabriel Dechichi <gabrieldechichi@gmail.com>
2025-02-22 21:30:37 -0800

    pacman.c: flip_x and flip_y

--- gamedev/pacman.c/main.c
+++ gamedev/pacman.c/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "rom.c"
 #include "typedefs.h"
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -26,6 +27,8 @@ typedef struct {
 typedef struct {
   uint32_t tile_code;
   uint32_t color_code;
+  bool8 flip_x;
+  bool8 flip_y;
 } PacmanSprite;
 
 global GameState game_state;
@@ -77,7 +80,7 @@ void draw_all_sprites() {
   for (size_t i = 0; i < NUM_SPRITES; i++) {
     PacmanSprite sprite = {};
     sprite.tile_code = i;
-    sprite.color_code = COLOR_PACMAN;
+    sprite.color_code = 14;
 
     draw_sprite(x, y, sprite);
     x += SPRITE_SIZE;
@@ -102,15 +105,50 @@ void draw_from_atlas(uint16_t offset_x, uint16_t offset_y, uint8_t *atlas,
 }
 
 void draw_sprite(uint16_t sprite_x, uint16_t sprite_y, PacmanSprite sprite) {
-  draw_from_atlas(sprite_x, sprite_y, (uint8_t *)game_state.rom.sprite_atlas,
-                  TILE_TEXTURE_WIDTH, SPRITE_SIZE,
-                  sprite.tile_code * SPRITE_SIZE, sprite.color_code);
+  // draw_from_atlas(sprite_x, sprite_y, (uint8_t *)game_state.rom.sprite_atlas,
+  //                 TILE_TEXTURE_WIDTH, SPRITE_SIZE,
+  //                 sprite.tile_code * SPRITE_SIZE, sprite.color_code);
+
+  uint32_t tile_code = sprite.tile_code * SPRITE_SIZE;
+  uint32_t color_code = sprite.color_code;
+
+  uint8_t tile_offset_x = sprite.flip_x ? SPRITE_SIZE : 0;
+  int8_t sign_x = sprite.flip_x ? -1 : 1;
+
+  uint8_t tile_offset_y = sprite.flip_y ? SPRITE_SIZE - 1 : 0;
+  int8_t sign_y = sprite.flip_y ? -1 : 1;
+  for (uint16_t y = 0; y < SPRITE_SIZE; y++) {
+    for (uint16_t x = 0; x < SPRITE_SIZE; x++) {
+      uint8_t tile_i =
+          game_state.rom.sprite_atlas[tile_offset_y + y * sign_y]
+                                     [tile_code + tile_offset_x + sign_x * x];
+      uint8_t color_i = color_code * 4 + tile_i;
+      uint32_t color = game_state.rom.color_palette[color_i];
+      game_state.frame_buffer[y + sprite_y][x + sprite_x] = color;
+    }
+  }
 }
 
 void draw_tile(uint16_t tile_x, uint16_t tile_y, uint32_t tile_code,
                uint8_t color_code) {
   draw_from_atlas(tile_x, tile_y, (uint8_t *)game_state.rom.tile_atlas,
                   TILE_TEXTURE_WIDTH, TILE_SIZE, tile_code, color_code);
+}
+
+void render_frame_buffer() {
+  for (uint16_t y = 0; y < DISPLAY_RES_Y; y++) {
+    for (uint16_t x = 0; x < DISPLAY_RES_X; x++) {
+      uint32_t color = game_state.frame_buffer[y][x];
+      Color rl_color = u32_to_color(color);
+      for (uint16_t yy = 0; yy < PIXEL_SCALE; yy++) {
+        for (uint16_t xx = 0; xx < PIXEL_SCALE; xx++) {
+          uint16_t py = y * PIXEL_SCALE + yy;
+          uint16_t px = x * PIXEL_SCALE + xx;
+          DrawPixel(px, py, rl_color);
+        }
+      }
+    }
+  }
 }
 
 int main(void) {
@@ -120,28 +158,17 @@ int main(void) {
 
   pm_init_rom(&game_state.rom);
 
+  static const uint8_t tiles[2][4] = {
+      {44, 46, 48, 46}, // horizontal (needs flipx)
+      {45, 47, 48, 47}  // vertical (needs flipy)
+  };
+
   while (!WindowShouldClose()) {
     BeginDrawing();
 
     ClearBackground(BLACK);
-
-    // draw_color_palette();
-    // draw_sprite(10, 100, sprite_pacman);
-    draw_all_sprites();
-
-    for (uint16_t y = 0; y < DISPLAY_RES_Y; y++) {
-      for (uint16_t x = 0; x < DISPLAY_RES_X; x++) {
-        uint32_t color = game_state.frame_buffer[y][x];
-        Color rl_color = u32_to_color(color);
-        for (uint16_t yy = 0; yy < PIXEL_SCALE; yy++) {
-          for (uint16_t xx = 0; xx < PIXEL_SCALE; xx++) {
-            uint16_t py = y * PIXEL_SCALE + yy;
-            uint16_t px = x * PIXEL_SCALE + xx;
-            DrawPixel(px, py, rl_color);
-          }
-        }
-      }
-    }
+    draw_sprite(10, 100, (PacmanSprite){tiles[1][0], COLOR_PACMAN, TRUE, TRUE});
+    render_frame_buffer();
 
     EndDrawing();
   }
--- gamedev/pacman.c/typedefs.h
+++ gamedev/pacman.c/typedefs.h
@@ -7,3 +7,8 @@
 #define ARRAY_SIZE(arr) (size_t)(sizeof(arr) / sizeof(arr[0]))
 
 #define XY_TO_INDEX(x, y, w) (y) * (w) + (x)
+
+typedef unsigned char bool8;
+
+#define TRUE 1
+#define FALSE 0
