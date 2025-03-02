eaead60ef03ffc76cddaac5e3e5eaf90646d44c6
Gabriel Dechichi <gabrieldechichi@gmail.com>
2025-03-02 15:20:46 -0800

    sdl static linking

--- gamedev/pacman.c/main2.c
+++ gamedev/pacman.c/main2.c
@@ -1,56 +1,61 @@
-#include "SDL3/SDL.h"
-#include "SDL3/SDL_video.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_keycode.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3/SDL_render.h"
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
-    }
+int main() {
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
 
-    SDL_Window* window = SDL_CreateWindow("SDL3 Window", 640, 480, 0);
-    if (window == NULL) {
-        SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_ShowWindow(window);
+  SDL_Window *window = SDL_CreateWindow("SDL3 Window", 640, 480, 0);
+  if (window == NULL) {
+    SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+  SDL_ShowWindow(window);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-    if (renderer == NULL) {
-        SDL_Log("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    bool quit = false;
-    SDL_Event event;
-
-    while (!quit) {
-        // Event handling
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_EVENT_QUIT:
-                    quit = true;
-                    break;
-            }
-        }
-
-        // Clear the screen
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        // Drawing logic goes here
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderLine(renderer, 0, 0, 640, 480);
-
-        // Present the renderer
-        SDL_RenderPresent(renderer);
-    }
-
-    // Cleanup
-    SDL_DestroyRenderer(renderer);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+  if (renderer == NULL) {
+    SDL_Log("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
     SDL_DestroyWindow(window);
     SDL_Quit();
+    return 1;
+  }
 
-    return 0;
+  bool quit = false;
+  SDL_Event event;
+
+  while (!quit) {
+    // Event handling
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_EVENT_QUIT: {
+        quit = true;
+        break;
+      }
+      case SDL_EVENT_KEY_DOWN: {
+        if (event.key.key == SDLK_ESCAPE) {
+          quit = true;
+        }
+        break;
+      }
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderLine(renderer, 0, 0, 640, 480);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
