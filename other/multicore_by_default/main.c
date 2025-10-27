51b06b4d9471a797b1df9c66928a32f37171b3ff
Gabriel Dechichi <gabrieldechichi@gmail.com>
2025-10-26 17:25:57 -0700

    multicore: initial example

---
+++ other/multicore_by_default/main.c
@@ -0,0 +1,75 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+#include "unistd.h"
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+#define unused(x) (void)(x)
+
+typedef struct ThreadContext {
+  u8 thread_idx;
+  u8 thread_count;
+} ThreadContext;
+
+void entrypoint(ThreadContext *ctx) {
+  unused(ctx);
+  // Create test array
+  u64 array_size = 100000000;
+  i64 *array = malloc(array_size * sizeof(i64));
+  for (u64 i = 0; i < array_size; i++) {
+    array[i] = i + 1; // 1, 2, 3, ...
+  }
+
+  // Compute sum
+  i64 sum = 0;
+  for (u64 i = 0; i < array_size; i++) {
+    sum += array[i];
+  }
+
+  printf("Sum: %lld\n", (u64)sum);
+
+  free(array);
+}
+
+void *entrypoint_internal(void *arg) {
+  ThreadContext *ctx = (ThreadContext *)arg;
+  entrypoint(ctx);
+  return NULL;
+}
+
+i8 os_core_count() { return (i8)sysconf(_SC_NPROCESSORS_ONLN); }
+
+int main(void) {
+  const u8 thread_mult = 1;
+  i8 core_count = os_core_count();
+  u8 thread_count = core_count * thread_mult;
+
+  printf("Core count %d Thread count %d\n", core_count, thread_count);
+  pthread_t *threads = malloc(thread_count * sizeof(pthread_t));
+  ThreadContext *ctx_arr = malloc(thread_count * sizeof(ThreadContext));
+
+  for (u8 i = 0; i < thread_count; i++) {
+    ctx_arr[i] = (ThreadContext){
+        .thread_idx = i,
+        .thread_count = thread_count,
+    };
+    pthread_create(&threads[i], NULL, entrypoint_internal, &ctx_arr[i]);
+  }
+
+  for (u8 i = 0; i < thread_count; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  return 0;
+}
