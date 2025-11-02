441a365bf272031cc442dd025a7b58d06f5dc5cc
Gabriel Dechichi <gabrieldechichi@gmail.com>
2025-11-01 17:31:50 -0700

    clay: cleanup render loop

--- clay-backend/src/main.c
+++ clay-backend/src/main.c
@@ -1,35 +1,25 @@
-// Clay WebGL2 Backend - WASM Entry Point
-// Freestanding environment (no stdlib)
+#include "vendor.c"
 
-// External JS function we can call for logging
-extern void os_log(const char *str, int len);
+#define WASM_EXPORT(name) __attribute__((export_name(name)))
+#define UNUSED(x) ((void)(x))
+#define ARRAY_LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+#define CSTR_LEN(str) ((sizeof(str) / sizeof(str[0])) - 1)
 
-// Heap base provided by the linker
+extern void _os_log(const char *str, int len);
 extern unsigned char __heap_base;
 
-// Export heap base getter
-__attribute__((export_name("os_get_heap_base")))
-void *os_get_heap_base(void) {
-    return &__heap_base;
+#define os_log(cstr) _os_log(cstr, CSTR_LEN(cstr));
+
+WASM_EXPORT("os_get_heap_base") void *os_get_heap_base(void) {
+  return &__heap_base;
 }
 
-// Export the entrypoint function to JavaScript
-__attribute__((export_name("entrypoint")))
-int entrypoint(void) {
-    // Return a test value to verify WASM is working
-    // Later this will initialize Clay and setup rendering
-    const char *msg = "Entrypoint called!";
-    int len = 19; // length of message
-    os_log(msg, len);
-    return 1337;
+WASM_EXPORT("entrypoint") void entrypoint(void) {
+  os_log("Entrypoint called!");
 }
 
 // Main update and render function called each frame
-__attribute__((export_name("update_and_render")))
-void update_and_render(float delta_time) {
-    // For now, just log to verify the render loop is working
-    const char *msg = "Update frame";
-    int len = 12; // length of message
-    os_log(msg, len);
+WASM_EXPORT("update_and_render") void update_and_render(void* memory) {
+  UNUSED(memory);
+  os_log("Update frame");
 }
-
--- clay-backend/web/main.js
+++ clay-backend/web/main.js
@@ -1,260 +1,258 @@
 class WasmMemoryInterface {
-    constructor(memory) {
-        this.memory = memory;
-        // Size (in bytes) of the integer type, should be 4 on `js_wasm32` and 8 on `js_wasm64p32`
-        this.intSize = 4;
+  constructor(memory) {
+    this.memory = memory;
+    // Size (in bytes) of the integer type, should be 4 on `js_wasm32` and 8 on `js_wasm64p32`
+    this.intSize = 4;
+  }
+
+  get mem() {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new DataView(this.memory.buffer);
+  }
+
+  loadF32Array(addr, len) {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new Float32Array(this.memory.buffer, addr, len);
+  }
+
+  loadF64Array(addr, len) {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new Float64Array(this.memory.buffer, addr, len);
+  }
+
+  loadU32Array(addr, len) {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new Uint32Array(this.memory.buffer, addr, len);
+  }
+
+  loadI32Array(addr, len) {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new Int32Array(this.memory.buffer, addr, len);
+  }
+
+  loadI16Array(addr, len) {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new Int16Array(this.memory.buffer, addr, len);
+  }
+
+  loadU8(addr) {
+    return this.mem.getUint8(addr);
+  }
+  loadI8(addr) {
+    return this.mem.getInt8(addr);
+  }
+  loadU16(addr) {
+    return this.mem.getUint16(addr, true);
+  }
+  loadI16(addr) {
+    return this.mem.getInt16(addr, true);
+  }
+  loadU32(addr) {
+    return this.mem.getUint32(addr, true);
+  }
+  loadI32(addr) {
+    return this.mem.getInt32(addr, true);
+  }
+  loadU64(addr) {
+    const lo = this.mem.getUint32(addr + 0, true);
+    const hi = this.mem.getUint32(addr + 4, true);
+    return lo + hi * 4294967296;
+  }
+  loadI64(addr) {
+    const lo = this.mem.getUint32(addr + 0, true);
+    const hi = this.mem.getInt32(addr + 4, true);
+    return lo + hi * 4294967296;
+  }
+  loadF32(addr) {
+    return this.mem.getFloat32(addr, true);
+  }
+  loadF64(addr) {
+    return this.mem.getFloat64(addr, true);
+  }
+  loadInt(addr) {
+    if (this.intSize === 8) {
+      return this.loadI64(addr);
+    } else if (this.intSize === 4) {
+      return this.loadI32(addr);
+    } else {
+      throw new Error("Unhandled `intSize`, expected `4` or `8`");
+    }
+  }
+  loadUint(addr) {
+    if (this.intSize === 8) {
+      return this.loadU64(addr);
+    } else if (this.intSize === 4) {
+      return this.loadU32(addr);
+    } else {
+      throw new Error("Unhandled `intSize`, expected `4` or `8`");
+    }
+  }
+  loadPtr(addr) {
+    return this.loadU32(addr);
+  }
+
+  loadB32(addr) {
+    return this.loadU32(addr) !== 0;
+  }
+
+  loadBytes(ptr, len) {
+    if (!this.memory) {
+      throw new Error("Memory not set");
+    }
+    return new Uint8Array(this.memory.buffer, ptr, Number(len));
+  }
+
+  loadString(ptr, len) {
+    const bytes = this.loadBytes(ptr, Number(len));
+    return new TextDecoder().decode(bytes);
+  }
+
+  loadCstring(ptr) {
+    return this.loadCstringDirect(this.loadPtr(ptr));
+  }
+
+  loadCstringDirect(start) {
+    if (start === 0) {
+      return null;
+    }
+    let len = 0;
+    for (; this.mem.getUint8(start + len) !== 0; len += 1) {}
+    return this.loadString(start, len);
+  }
+
+  storeU8(addr, value) {
+    this.mem.setUint8(addr, value);
+  }
+  storeI8(addr, value) {
+    this.mem.setInt8(addr, value);
+  }
+  storeU16(addr, value) {
+    this.mem.setUint16(addr, value, true);
+  }
+  storeI16(addr, value) {
+    this.mem.setInt16(addr, value, true);
+  }
+  storeU32(addr, value) {
+    this.mem.setUint32(addr, value, true);
+  }
+  storeI32(addr, value) {
+    this.mem.setInt32(addr, value, true);
+  }
+  storeU64(addr, value) {
+    this.mem.setUint32(addr + 0, Number(value), true);
+
+    let div = 4294967296;
+    if (typeof value === "bigint") {
+      div = BigInt(div);
     }
 
-    get mem() {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new DataView(this.memory.buffer);
+    this.mem.setUint32(addr + 4, Math.floor(Number(value / div)), true);
+  }
+  storeI64(addr, value) {
+    this.mem.setUint32(addr + 0, Number(value), true);
+
+    let div = 4294967296;
+    if (typeof value === "bigint") {
+      div = BigInt(div);
     }
 
-    loadF32Array(addr, len) {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new Float32Array(this.memory.buffer, addr, len);
+    this.mem.setInt32(addr + 4, Math.floor(Number(value / div)), true);
+  }
+  storeF32(addr, value) {
+    this.mem.setFloat32(addr, value, true);
+  }
+  storeF64(addr, value) {
+    this.mem.setFloat64(addr, value, true);
+  }
+  storeInt(addr, value) {
+    if (this.intSize === 8) {
+      this.storeI64(addr, value);
+    } else if (this.intSize === 4) {
+      this.storeI32(addr, value);
+    } else {
+      throw new Error("Unhandled `intSize`, expected `4` or `8`");
     }
+  }
+  storeUint(addr, value) {
+    if (this.intSize === 8) {
+      this.storeU64(addr, value);
+    } else if (this.intSize === 4) {
+      this.storeU32(addr, value);
+    } else {
+      throw new Error("Unhandled `intSize`, expected `4` or `8`");
+    }
+  }
 
-    loadF64Array(addr, len) {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new Float64Array(this.memory.buffer, addr, len);
-    }
-
-    loadU32Array(addr, len) {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new Uint32Array(this.memory.buffer, addr, len);
-    }
-
-    loadI32Array(addr, len) {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new Int32Array(this.memory.buffer, addr, len);
-    }
-
-    loadI16Array(addr, len) {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new Int16Array(this.memory.buffer, addr, len);
-    }
-
-    loadU8(addr) {
-        return this.mem.getUint8(addr);
-    }
-    loadI8(addr) {
-        return this.mem.getInt8(addr);
-    }
-    loadU16(addr) {
-        return this.mem.getUint16(addr, true);
-    }
-    loadI16(addr) {
-        return this.mem.getInt16(addr, true);
-    }
-    loadU32(addr) {
-        return this.mem.getUint32(addr, true);
-    }
-    loadI32(addr) {
-        return this.mem.getInt32(addr, true);
-    }
-    loadU64(addr) {
-        const lo = this.mem.getUint32(addr + 0, true);
-        const hi = this.mem.getUint32(addr + 4, true);
-        return lo + hi * 4294967296;
-    }
-    loadI64(addr) {
-        const lo = this.mem.getUint32(addr + 0, true);
-        const hi = this.mem.getInt32(addr + 4, true);
-        return lo + hi * 4294967296;
-    }
-    loadF32(addr) {
-        return this.mem.getFloat32(addr, true);
-    }
-    loadF64(addr) {
-        return this.mem.getFloat64(addr, true);
-    }
-    loadInt(addr) {
-        if (this.intSize === 8) {
-            return this.loadI64(addr);
-        } else if (this.intSize === 4) {
-            return this.loadI32(addr);
-        } else {
-            throw new Error("Unhandled `intSize`, expected `4` or `8`");
-        }
-    }
-    loadUint(addr) {
-        if (this.intSize === 8) {
-            return this.loadU64(addr);
-        } else if (this.intSize === 4) {
-            return this.loadU32(addr);
-        } else {
-            throw new Error("Unhandled `intSize`, expected `4` or `8`");
-        }
-    }
-    loadPtr(addr) {
-        return this.loadU32(addr);
-    }
-
-    loadB32(addr) {
-        return this.loadU32(addr) !== 0;
-    }
-
-    loadBytes(ptr, len) {
-        if (!this.memory) {
-            throw new Error("Memory not set");
-        }
-        return new Uint8Array(this.memory.buffer, ptr, Number(len));
-    }
-
-    loadString(ptr, len) {
-        const bytes = this.loadBytes(ptr, Number(len));
-        return new TextDecoder().decode(bytes);
-    }
-
-    loadCstring(ptr) {
-        return this.loadCstringDirect(this.loadPtr(ptr));
-    }
-
-    loadCstringDirect(start) {
-        if (start === 0) {
-            return null;
-        }
-        let len = 0;
-        for (; this.mem.getUint8(start + len) !== 0; len += 1) { }
-        return this.loadString(start, len);
-    }
-
-    storeU8(addr, value) {
-        this.mem.setUint8(addr, value);
-    }
-    storeI8(addr, value) {
-        this.mem.setInt8(addr, value);
-    }
-    storeU16(addr, value) {
-        this.mem.setUint16(addr, value, true);
-    }
-    storeI16(addr, value) {
-        this.mem.setInt16(addr, value, true);
-    }
-    storeU32(addr, value) {
-        this.mem.setUint32(addr, value, true);
-    }
-    storeI32(addr, value) {
-        this.mem.setInt32(addr, value, true);
-    }
-    storeU64(addr, value) {
-        this.mem.setUint32(addr + 0, Number(value), true);
-
-        let div = 4294967296;
-        if (typeof value === "bigint") {
-            div = BigInt(div);
-        }
-
-        this.mem.setUint32(addr + 4, Math.floor(Number(value / div)), true);
-    }
-    storeI64(addr, value) {
-        this.mem.setUint32(addr + 0, Number(value), true);
-
-        let div = 4294967296;
-        if (typeof value === "bigint") {
-            div = BigInt(div);
-        }
-
-        this.mem.setInt32(addr + 4, Math.floor(Number(value / div)), true);
-    }
-    storeF32(addr, value) {
-        this.mem.setFloat32(addr, value, true);
-    }
-    storeF64(addr, value) {
-        this.mem.setFloat64(addr, value, true);
-    }
-    storeInt(addr, value) {
-        if (this.intSize === 8) {
-            this.storeI64(addr, value);
-        } else if (this.intSize === 4) {
-            this.storeI32(addr, value);
-        } else {
-            throw new Error("Unhandled `intSize`, expected `4` or `8`");
-        }
-    }
-    storeUint(addr, value) {
-        if (this.intSize === 8) {
-            this.storeU64(addr, value);
-        } else if (this.intSize === 4) {
-            this.storeU32(addr, value);
-        } else {
-            throw new Error("Unhandled `intSize`, expected `4` or `8`");
-        }
-    }
-
-    // Returned length might not be the same as `value.length` if non-ascii strings are given.
-    storeString(addr, value) {
-        const src = new TextEncoder().encode(value);
-        const dst = new Uint8Array(this.memory.buffer, addr, src.length);
-        dst.set(src);
-        return src.length;
-    }
+  // Returned length might not be the same as `value.length` if non-ascii strings are given.
+  storeString(addr, value) {
+    const src = new TextEncoder().encode(value);
+    const dst = new Uint8Array(this.memory.buffer, addr, src.length);
+    dst.set(src);
+    return src.length;
+  }
 }
 
-const canvas = document.getElementById('canvas');
-const gl = canvas.getContext('webgl2');
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl2");
 
 const memory = new WebAssembly.Memory({ initial: 256, maximum: 256 });
 const memInterface = new WasmMemoryInterface(memory);
 
 const importObject = {
-    env: {
-        memory: memory,
-        os_log: (ptr, len) => {
-            const message = memInterface.loadString(ptr, len);
-            console.log('[WASM]:', message);
-        },
-    }
+  env: {
+    memory: memory,
+    _os_log: (ptr, len) => {
+      const message = memInterface.loadString(ptr, len);
+      console.log("[WASM]:", message);
+    },
+  },
 };
 
 let wasmExports = null;
-let heapBase = null;
+let heapBase = 0;
 let previousFrameTime = 0;
 
 function renderLoop(currentTime) {
-    // Calculate delta time in seconds
-    const deltaTime = previousFrameTime
-        ? (currentTime - previousFrameTime) / 1000
-        : 0;
-    previousFrameTime = currentTime;
+  // Calculate delta time in seconds
+  const deltaTime = previousFrameTime
+    ? (currentTime - previousFrameTime) / 1000
+    : 0;
+  previousFrameTime = currentTime;
 
-    // Call WASM update_and_render
-    wasmExports.update_and_render(deltaTime);
+  // Call WASM update_and_render
+  wasmExports.update_and_render(heapBase);
 
-    // Request next frame
-    requestAnimationFrame(renderLoop);
+  // Request next frame
+  requestAnimationFrame(renderLoop);
 }
 
 async function loadWasm() {
-    const response = await fetch('app.wasm');
-    const wasmBytes = await response.arrayBuffer();
-    const wasmModule = await WebAssembly.instantiate(wasmBytes, importObject);
+  const response = await fetch("app.wasm");
+  const wasmBytes = await response.arrayBuffer();
+  const wasmModule = await WebAssembly.instantiate(wasmBytes, importObject);
 
-    const result = wasmModule.instance.exports.entrypoint();
-    console.log('entrypoint() returned:', result);
+  wasmModule.instance.exports.entrypoint();
 
-    wasmExports = wasmModule.instance.exports;
-    window.wasmExports = wasmExports;
+  wasmExports = wasmModule.instance.exports;
+  window.wasmExports = wasmExports;
 
-    // Get heap base pointer
-    heapBase = wasmExports.os_get_heap_base();
-    console.log('Heap base:', heapBase);
+  // Get heap base pointer
+  heapBase = wasmExports.os_get_heap_base();
 
-    // Start the render loop
-    requestAnimationFrame(renderLoop);
+  // Start the render loop
+  requestAnimationFrame(renderLoop);
 }
 
 loadWasm();
