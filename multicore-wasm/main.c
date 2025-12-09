62e1dd5287a152b2f6d36824d19f05674121f703
Gabriel Dechichi <gabrieldechichi@gmail.com>
2025-12-09 13:53:16 -0800

    multicore-wasm: automate testing of thread stuff

--- multicore-wasm/main.c
+++ multicore-wasm/main.c
@@ -4,7 +4,7 @@
 #include "lib/typedefs.h"
 
 // === SELECT ONE DEMO ===
-#include "demos/demo_thread_create.c"   // 1. Basic thread create/join
+// #include "demos/demo_thread_create.c"   // 1. Basic thread create/join
 // #include "demos/demo_tls.c"          // 2. Thread Local Storage
 // #include "demos/demo_shared_memory.c" // 3. Shared memory + race conditions
 // #include "demos/demo_mutex.c"        // 4. Mutex lock/unlock
@@ -15,7 +15,7 @@
 // #include "demos/demo_rwlock.c"       // 9. Read-write lock
 // #include "demos/demo_detach.c"       // 10. Thread detach
 // #include "demos/demo_thread_attr.c"  // 11. Thread attributes
-// #include "demos/demo_once.c"         // 12. pthread_once
+#include "demos/demo_once.c"         // 12. pthread_once
 
 int main(void) {
     return demo_main();
